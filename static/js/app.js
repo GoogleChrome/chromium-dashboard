5426e1571f4cb19eee9b0031af50f248c6f18ba0
Eric Bidelman <ebidel@gmail.com>
2013-08-28 14:11:17 -0700

    Adding xhr.responseType="json"

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,26 @@
+var $ = function(selector) {
+  return document.querySelector(selector);
+};
+
+var $$ = function(selector) {
+  return document.querySelectorAll(selector);
+};
+
+function testXhrType(type) {
+  if (typeof XMLHttpRequest == 'undefined') {
+    return false;
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/', true);
+  try {
+    xhr.responseType = type;
+  } catch(error) {
+    return false;
+  }
+  return 'response' in xhr && xhr.responseType == type;
+}
+
 document.addEventListener('WebComponentsReady', function(e) {
   // // Add .resolved to all custom elements. This is a hack until :unknown is supported
   // // in browsers and Polymer registers elements using document.register().
