8ef335411877749622190eca7c82684bf7cdc930
Mounir Lamouri <mounir@lamouri.fr>
2014-06-11 21:11:12 +0100

    Intent to implement should not include the edit URL.

--- admin.py
+++ admin.py
@@ -275,6 +275,8 @@ class FeatureHandler(common.ContentHandler):
       template_data.update({
           'feature': f.format_for_template(),
           'feature_form': models.FeatureForm(f.format_for_edit()),
+          'default_url': '%s://%s%s/%s' % (self.request.scheme, self.request.host,
+                                           self.DEFAULT_URL, feature_id)
           'edit_url': '%s://%s%s/%s' % (self.request.scheme, self.request.host,
                                         self.EDIT_URL, feature_id)
           })
