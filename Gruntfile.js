9a78b0797cbca35eb403dd0d413d4fcc265346ff
Eric Bidelman <ebidel@gmail.com>
2014-10-01 18:59:32 -0700

    No longer need element names in registration

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         //   ]
         // },
         strip: true,
-        csp: true, // TODO: update node_modules flip this
+        csp: true,
         inline: true
       },
       build: {
