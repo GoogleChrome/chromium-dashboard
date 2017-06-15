8f96a9304e9208b8436b64c386ba972b8bdb58c4
Eric Bidelman <ebidel@gmail.com>
2017-06-15 13:47:28 -0700

    Features appearing on release cards

--- schedule.py
+++ schedule.py
@@ -27,26 +27,43 @@ import webapp2
 
 # Appengine imports.
 # import cloudstorage
-# from google.appengine.api import memcache
+from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.api import users
 # from google.appengine.ext import blobstore
 # from google.appengine.ext import db
 # from google.appengine.ext.webapp import blobstore_handlers
 
-# File imports.
 import common
-# import models
+import models
 import settings
 import util
 
 
 def fetch_chrome_release_info(version):
-  url = 'https://chromepmo.appspot.com/schedule/mstone/json?mstone=%s' % version
-  result = urlfetch.fetch(url)
-  if result.status_code == 200:
-    return json.loads(result.content)['mstones']
-  return None
+  key = '%s|chromerelease|%s' % (settings.MEMCACHE_KEY_PREFIX, version)
+
+  data = memcache.get(key)
+  if data is None:
+    url = 'https://chromepmo.appspot.com/schedule/mstone/json?mstone=%s' % version
+    result = urlfetch.fetch(url, deadline=60)
+    if result.status_code == 200:
+      data = json.loads(result.content)['mstones'][0]
+      del data['owners']
+      del data['feature_freeze']
+      del data['ldaps']
+      memcache.set(key, data)
+  return data
+
+def construct_chrome_channels_details(omaha_data):
+  channels = {}
+  win_versions = omaha_data[0]['versions']
+  for v in win_versions:
+    channel = v['channel']
+    major_version = int(v['version'].split('.')[0])
+    channels[channel] = fetch_chrome_release_info(major_version)
+    channels[channel]['version'] = major_version
+  return channels
 
 
 class ScheduleHandler(common.ContentHandler):
@@ -57,13 +74,19 @@ class ScheduleHandler(common.ContentHandler):
       common.handle_401(self.request, self.response, Exception)
       return
 
+    omaha_data = util.get_omaha_data()
+
+    # features = Feature.get_chronological()
+    # for f in features:
+      # milesstone = f.get('meta').get('milestone_str')
+    #  print f.get('impl_status_chrome')
+
     data = {
-      'versions': json.dumps(util.get_omaha_data()),
-      'release': fetch_chrome_release_info(58)
+      'features': json.dumps(models.Feature.get_chronological()),
+      'channels': json.dumps(construct_chrome_channels_details(omaha_data))
     }
 
     self.render(data, template_path=os.path.join(path + '.html'))
-    # self.response.out.write('here')
 
 
 app = webapp2.WSGIApplication([
