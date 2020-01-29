88f524f9500bbad03431e0263403d24b19d4e9f2
Jason Robbins <jrobbins@chromium.org>
2020-01-28 19:39:22 -0500

    trying before_install

--- travis/install_google_cloud_sdk.sh
+++ travis/install_google_cloud_sdk.sh
@@ -8,10 +8,12 @@ openssl aes-256-cbc -K $encrypted_aee7e38c959c_key -iv $encrypted_aee7e38c959c_i
     -in gcloud-client-secret.json.enc -out gcloud-client-secret.json -d
 
 # Download & install the Google Cloud SDK
-curl https://sdk.cloud.google.com | bash
+#curl https://sdk.cloud.google.com | bash
 
 # Update any necessary components
 $HOME/google-cloud-sdk/bin/gcloud components update -q
+$HOME/google-cloud-sdk/bin/gcloud components install app-engine-python
+
 
 # Set the AppEngine App ID to $GAE_APP_ID
 $HOME/google-cloud-sdk/bin/gcloud config set project $GAE_APP_ID
