0bf0f61ae56a5477fe0d06169f79a44e6d012d68
Jason Robbins <jrobbins@chromium.org>
2020-01-30 13:49:00 -0500

    Remove commented out code

--- travis/install_google_cloud_sdk.sh
+++ travis/install_google_cloud_sdk.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -e
 
-# Installs the Google Cloud SDK
+# Note: We now install and update the Google Cloud SDK in .travis.yml.
+# The rest of this file does configuraton and authorization steps.
 
 # Decrypt the Service Account Key
 openssl aes-256-cbc -K $encrypted_aee7e38c959c_key -iv $encrypted_aee7e38c959c_iv \
     -in gcloud-client-secret.json.enc -out gcloud-client-secret.json -d
-
-# Download & install the Google Cloud SDK
-#curl https://sdk.cloud.google.com | bash
-
-# Update any necessary components
-#$HOME/google-cloud-sdk/bin/gcloud components update -q
-#$HOME/google-cloud-sdk/bin/gcloud components install app-engine-python
-
 
 # Set the AppEngine App ID to $GAE_APP_ID
 $HOME/google-cloud-sdk/bin/gcloud config set project $GAE_APP_ID
