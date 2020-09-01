00631812d1aab5fb9f8a3bd5bb4489f237fda9c4
Jason Robbins <jrobbins@gmail.com>
2020-09-01 19:52:28 -0400

    Fix the guide cancel buttons

--- static/js-src/admin/feature_form.js
+++ static/js-src/admin/feature_form.js
@@ -48,7 +48,7 @@ if (document.querySelector('.delete-button')) {
 
 if (document.querySelector('#cancel-button')) {
   document.querySelector('#cancel-button').addEventListener('click', (e) => {
-    location.href = `/guide/edit/${e.currentTarget.dataset.id}`;
+    window.location.href = `/guide/edit/${e.currentTarget.dataset.id}`;
   });
 }
 
