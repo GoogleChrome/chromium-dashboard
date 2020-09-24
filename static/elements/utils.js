1760f15cbeaa98e6e72cc1927250ef9ef7bd3a40
Jason Robbins <jrobbins@gmail.com>
2020-09-24 15:04:41 -0400

    Fix urlize() usage for feature comments by defining autolink().

---
+++ static/elements/utils.js
@@ -0,0 +1,15 @@
+// This file contains helper functions for our elements.
+
+import {html} from 'lit-element';
+import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
+
+/* Convert user-entered text into safe HTML with clickable links
+ * where appropriate.  Returns a lit-html TemplateResult.
+ */
+// TODO(jrobbins): autolink monorail-style issue references, go-links, etc.
+export function autolink(s) {
+  const markup = urlize(
+    s,
+    {target: '_blank', trim: 'www', autoescape: true});
+  return html`${unsafeHTML(markup)}`;
+}
