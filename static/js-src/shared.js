d8af86d803d8102be888e79a2271c4da547f65e0
Jason Robbins <jrobbins@gmail.com>
2020-09-30 09:42:27 -0700

    Use new Google Analytics account. (#1047)

--- static/js-src/shared.js
+++ static/js-src/shared.js
@@ -18,12 +18,10 @@
 
 // Google Analytics
 /* eslint-disable */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-/* eslint-enable */
+// Global site tag (gtag.js) - Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-ga('create', 'UA-39048143-1', 'auto');
-ga('send', 'pageview');
+gtag('config', 'UA-179341418-1');
 // End Google Analytics
