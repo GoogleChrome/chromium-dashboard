48c0f512a75eccf8794dea75177ec3004be8944f
Jason Robbins <jrobbins@gmail.com>
2020-12-17 16:16:57 -0800

    remove fix_sys_path (#1119)
    * remove fix_sys_path
    
    * remove debugging print

--- appengine_config.py
+++ appengine_config.py
@@ -2,11 +2,9 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import sys
 # name of the django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from google.appengine.ext import vendor
 vendor.add('lib') # add third party libs to "lib" folder.
-
-import dev_appserver
-dev_appserver.fix_sys_path()
