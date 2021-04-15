aa8832700f292d419cf6fd2b4ec1867f213afef5
Jason Robbins <jrobbins@gmail.com>
2021-04-14 21:25:19 -0400

    Implement XSRF tokens (but not enforced yet).

---
+++ framework/xsrf.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import division
+from __future__ import print_function
+
+import base64
+import hmac
+import logging
+import random
+import string
+import time
+
+from google.appengine.ext import db
+
+from framework import constants
+
+
+# This is how long tokens are valid.
+TOKEN_TIMEOUT_SEC = 2 * constants.SECS_PER_HOUR
+
+# The token refresh servlet accepts old tokens to generate new ones, but
+# we still impose a limit on how old they can be.
+REFRESH_TOKEN_TIMEOUT_SEC = 10 * constants.SECS_PER_DAY
+
+# When the JS on a page decides whether or not it needs to refresh the
+# XSRF token before submitting a form, there could be some clock skew,
+# so we subtract a little time to avoid having the JS use an existing
+# token that the server might consider expired already.
+TOKEN_TIMEOUT_MARGIN_SEC = 5 * constants.SECS_PER_MINUTE
+
+# When checking that the token is not from the future, allow a little
+# margin for the possibliity that the clock of the GAE instance that
+# generated the token could be a little ahead of the one checking.
+CLOCK_SKEW_SEC = 5
+
+DELIMITER = ':'
+
+
+# For random key generation
+RANDOM_KEY_LENGTH = 128
+RANDOM_KEY_CHARACTERS = string.ascii_letters + string.digits
+
+
+def make_random_key(length=RANDOM_KEY_LENGTH, chars=RANDOM_KEY_CHARACTERS):
+  """Return a string with lots of random characters."""
+  chars = [random.choice(chars) for _ in range(length)]
+  return ''.join(chars)
+
+
+class Secrets(db.Model):
+  """A server-side-only value that we use to generate security tokens."""
+
+  xsrf_secret = db.StringProperty(required=True)
+
+  @classmethod
+  def _get_or_make_singleton(cls):
+    existing = Secrets.all().fetch(1)
+    if existing:
+      return existing[0]
+
+    logging.info('Creating new secrets')
+    xsrf_secret = make_random_key()
+    new_entity = Secrets(xsrf_secret=xsrf_secret)
+    new_entity.put()
+    return new_entity
+
+  @classmethod
+  def get_xsrf_secret(cls):
+    """Return the xsrf secret key."""
+    singleton = cls._get_or_make_singleton()
+    return singleton.xsrf_secret
+
+
+
+def generate_token(user_email, token_time=None):
+  """Return a security token specifically for the given user.
+  Args:
+    user_email: email addr of the user viewing an HTML form.  This can
+        be None for anon vistors.
+    token_time: Time at which the token is generated in seconds since the epoch.
+  Returns:
+    A url-safe security token.  The token is a string with the digest
+    the email and time, followed by plain-text copy of the time that is
+    used in validation.
+  Raises:
+    ValueError: if the XSRF secret was not configured.
+  """
+  token_time = token_time or int(time.time())
+  digester = hmac.new(Secrets.get_xsrf_secret())
+  digester.update(user_email or '')
+  digester.update(DELIMITER)
+  digester.update(str(token_time))
+  digest = digester.digest()
+  token = base64.urlsafe_b64encode('%s%s%d' % (digest, DELIMITER, token_time))
+  return token
+
+
+def validate_token(
+    token, user_email, timeout=TOKEN_TIMEOUT_SEC):
+  """Return True if the given token is valid for the given scope.
+  Args:
+    token: String token that was presented by the user.
+    user_email: user email addr.
+    timeout: int max token age in seconds.
+  Raises:
+    TokenIncorrect: if the token is missing or invalid.
+  """
+  if not token:
+    raise TokenIncorrect('missing token')
+  try:
+    decoded = base64.urlsafe_b64decode(str(token))
+    token_time = int(decoded.split(DELIMITER)[-1])
+  except (TypeError, ValueError):
+    raise TokenIncorrect('could not decode token')
+  now = int(time.time())
+
+  # The given token should match the generated one with the same time.
+  expected_token = generate_token(user_email, token_time=token_time)
+  if len(token) != len(expected_token):
+    raise TokenIncorrect('presented token is wrong size')
+
+  # Perform constant time comparison to avoid timing attacks
+  different = 0
+  for x, y in zip(token, expected_token):
+    different |= ord(x) ^ ord(y)
+  if different:
+    raise TokenIncorrect(
+        'presented token does not match expected token: %r != %r' % (
+            token, expected_token))
+
+  # We reject tokens from the future.
+  if token_time > now + CLOCK_SKEW_SEC:
+    raise TokenIncorrect('token is from future')
+
+  # We check expiration last so that we only raise the expriration error
+  # if the token would have otherwise been valid.
+  if now - token_time > timeout:
+    raise TokenIncorrect('token has expired')
+
+
+def token_expires_sec():
+  """Return timestamp when current tokens will expire, minus a safety margin."""
+  now = int(time.time())
+  return now + TOKEN_TIMEOUT_SEC - TOKEN_TIMEOUT_MARGIN_SEC
+
+
+class Error(Exception):
+  """Base class for errors from this module."""
+  pass
+
+
+# Caught separately in servlet.py
+class TokenIncorrect(Error):
+  """The POST body has an incorrect URL Command Attack token."""
+  pass
---
+++ framework/xsrf_test.py
@@ -0,0 +1,116 @@
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+import testing_config  # Must be imported before the module under test.
+
+import mock
+
+from framework import xsrf
+
+
+class XsrfTest(unittest.TestCase):
+  """Set of unit tests for blocking XSRF attacks."""
+
+  def test_make_random_key__long(self):
+    """The random keys have the desired length."""
+    key = xsrf.make_random_key()
+    self.assertEqual(xsrf.RANDOM_KEY_LENGTH, len(key))
+
+  def test_make_random_key__distinct(self):
+    """The random keys are different."""
+    key_set = set()
+    for i in range(1000):
+      key_set.add(xsrf.make_random_key())
+    self.assertEqual(1000, len(key_set))
+
+  def test_generate_token__anon(self):
+    """Anon users get a real token."""
+    self.assertNotEqual('', xsrf.generate_token(None))
+
+  def test_generate_token__distinct(self):
+    """Each user gets their own distinct token."""
+    self.assertNotEqual(
+        xsrf.generate_token('user1@example.com'),
+        xsrf.generate_token('user2@example.com'))
+
+    self.assertNotEqual(
+        xsrf.generate_token('user1@example.com'),
+        xsrf.generate_token(None))
+
+  def test_validate_token__normal(self):
+    """We accept valid tokens."""
+    token = xsrf.generate_token('user1@example.com')
+    xsrf.validate_token(token, 'user1@example.com')  # no exception raised
+
+  def test_validate_token__malformed_token(self):
+    """We reject missing or non-matching tokens."""
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token('bad', 'user1@example.com')
+
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token('', 'user1@example.com')
+
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token(
+          '098a08fe08b08c08a05e:9721973123',
+          'user1@example.com')
+
+  def test_validate_token__wrong_user(self):
+    """We reject a user attempting to use a different user's token."""
+    token = xsrf.generate_token('user1@example.com')
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token(token, 'user2@example.com')
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token(token, None)
+
+  @mock.patch('time.time')
+  def test_validate_token__expiration(self, mock_time):
+    """We accept non-expired tokens and reject expired ones."""
+    test_time = 1526671379
+    mock_time.return_value = test_time
+    token = xsrf.generate_token('user1@example.com')
+
+    xsrf.validate_token(token, 'user1@example.com')
+
+    mock_time.return_value = test_time + 1
+    xsrf.validate_token(token, 'user1@example.com')
+
+    mock_time.return_value = test_time + xsrf.TOKEN_TIMEOUT_SEC
+    xsrf.validate_token(token, 'user1@example.com')
+
+    mock_time.return_value = test_time + xsrf.TOKEN_TIMEOUT_SEC + 1
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token(token, 'user1@example.com')
+
+  @mock.patch('time.time')
+  def test_validate_token__future(self, mock_time):
+    """We reject tokens from the future."""
+    test_time = 1526671379
+    mock_time.return_value = test_time
+    token = xsrf.generate_token('user1@example.com')
+
+    xsrf.validate_token(token, 'user1@example.com')
+
+    # The clock of the GAE instance doing the checking might be slightly slow.
+    mock_time.return_value = test_time - 1
+    xsrf.validate_token(token, 'user1@example.com')
+
+    # But, if the difference is too much, someone is trying to fake a token.
+    mock_time.return_value = test_time - xsrf.CLOCK_SKEW_SEC - 1
+    with self.assertRaises(xsrf.TokenIncorrect):
+      xsrf.validate_token(token, 'user1@example.com')
