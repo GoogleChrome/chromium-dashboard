ad16723ab9f7d46ad555911129f2d366703b946a
Jason Robbins <jrobbins@gmail.com>
2021-04-27 21:43:36 -0400

    Implement JS client class for our API.

---
+++ static/js-src/cs-client.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2021 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function(exports) {
+'use strict';
+
+
+class ChromeStatusClient {
+  constructor(token, tokenExpiresSec) {
+    this.token = token;
+    this.tokenExpiresSec = tokenExpiresSec;
+    this.baseUrl = '/api/v0'; // Same scheme, host, and port.
+  }
+
+  /**
+   * Refresh the XSRF token if necessary.
+   */
+  async ensureTokenIsValid() {
+    if (ChromeStatusClient.isTokenExpired(this.tokenExpiresSec)) {
+      const refreshResponse = await this.doFetch(
+        '/currentuser/token', 'POST', null);
+      this.token = refreshResponse.token;
+      this.tokenExpiresSec = refreshResponse.tokenExpiresSec;
+    }
+  }
+
+  static isTokenExpired(tokenExpiresSec) {
+    const tokenExpiresDate = new Date(tokenExpiresSec * 1000);
+    return tokenExpiresDate < new Date();
+  }
+
+  async doFetch(resource, httpMethod, body, includeToken=true) {
+    const url = this.baseUrl + resource;
+    let headers = {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+    };
+    if (includeToken) {
+      headers['X-Xsrf-Token'] = this.token;
+    }
+    let options = {
+      method: httpMethod,
+      credentials: 'same-origin',
+      headers: headers,
+    };
+    if (body !== null) {
+      options['body'] = JSON.stringify(body);
+    }
+
+    const response = await fetch(url, options);
+
+    if (response.status !== 200) {
+      throw new Error(
+          `Got error response from server: ${response.status}`);
+    }
+    const rawResponseText = await response.text();
+    const XSSIPrefix = ')]}\'\n';
+    if (!rawResponseText.startsWith(XSSIPrefix)) {
+      console.log(rawResponseText);
+      throw new Error(
+          `Response does not start with XSSI prefix: ${XSSIPrefix}`);
+    }
+    return JSON.parse(rawResponseText.substr(XSSIPrefix.length));
+  }
+
+  doGet(resource, body) {
+    // GET's do not use token.
+    return this.doFetch(resource, 'GET', body, false);
+  }
+
+  doPost(resource, body) {
+    return this.ensureTokenIsValid().then(() => {
+      return this.doFetch(resource, 'POST', body);
+    });
+  }
+
+  doPatch(resource, body) {
+    return this.ensureTokenIsValid().then(() => {
+      return this.doFetch(resource, 'PATCH', body);
+    });
+  }
+
+  doDelete(resource) {
+    return this.ensureTokenIsValid().then(() => {
+      return this.doFetch(resource, 'DELETE', null);
+    });
+  }
+};
+
+exports.ChromeStatusClient = ChromeStatusClient;
+})(window);
