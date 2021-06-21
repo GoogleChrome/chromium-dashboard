7bd869f2e2aeab3a4cf4e3652d49f2e874b80659
shivamag00 <shivam.agrawal.2000@gmail.com>
2021-06-21 12:07:19 +0530

    Using Cloud NDB in testbed

--- testing_config.py
+++ testing_config.py
@@ -39,7 +39,8 @@ lib_path = os.path.join(os.path.dirname(__file__), 'lib')
 from google.appengine.ext import vendor
 vendor.add(lib_path) # add third party libs to "lib" folder.
 
-from google.appengine.ext import ndb
+# from google.appengine.ext import ndb
+from google.cloud import ndb
 from google.appengine.ext import testbed
 
 os.environ['DJANGO_SECRET'] = 'test secret'
