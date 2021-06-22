64f70e18f7817b15e6b4c5d920d867daa0eb58f2
shivamag00 <shivam.agrawal.2000@gmail.com>
2021-06-22 16:20:20 +0530

    Migrated approvals_api_test.py

--- api/approvals_api_test.py
+++ api/approvals_api_test.py
@@ -26,6 +26,9 @@ import werkzeug.exceptions  # Flask HTTP stuff.
 from api import register
 from api import approvals_api
 from internals import models
+from google.cloud import ndb
+
+client = ndb.Client()
 
 
 NOW = datetime.datetime.now()
@@ -37,7 +40,8 @@ class ApprovalsAPITest(unittest.TestCase):
     self.feature_1 = models.Feature(
         name='feature one', summary='sum', category=1, visibility=1,
         standardization=1, web_dev_views=1, impl_status_chrome=1)
-    self.feature_1.put()
+    with client.context():
+      self.feature_1.put()
     self.feature_id = self.feature_1.key.integer_id()
     self.handler = approvals_api.ApprovalsAPI()
     self.request_path = '/api/v0/features/%d/approvals' % self.feature_id
@@ -68,25 +72,29 @@ class ApprovalsAPITest(unittest.TestCase):
         }
 
   def tearDown(self):
-    self.feature_1.key.delete()
-    for appr in models.Approval.query():
-      appr.key.delete()
+    with client.context():
+      self.feature_1.key.delete()
+      for appr in models.Approval.query():
+        appr.key.delete()
 
   def test_get__all_empty(self):
     """We can get all approvals for a given feature, even if there none."""
     testing_config.sign_out()
     with register.app.test_request_context(self.request_path):
-      actual_response = self.handler.do_get(self.feature_id)
+      with client.context():
+        actual_response = self.handler.do_get(self.feature_id)
     self.assertEqual({"approvals": []}, actual_response)
 
   def test_get__all_some(self):
     """We can get all approvals for a given feature."""
     testing_config.sign_out()
-    self.appr_1_1.put()
-    self.appr_1_2.put()
+    with client.context():
+      self.appr_1_1.put()
+      self.appr_1_2.put()
 
     with register.app.test_request_context(self.request_path):
-      actual_response = self.handler.do_get(self.feature_id)
+      with client.context():
+        actual_response = self.handler.do_get(self.feature_id)
 
     self.assertEqual(
         {"approvals": [self.expected1, self.expected2]},
@@ -96,17 +104,20 @@ class ApprovalsAPITest(unittest.TestCase):
     """We can get approvals for given feature and field, even if there none."""
     testing_config.sign_out()
     with register.app.test_request_context(self.request_path + '/1'):
-      actual_response = self.handler.do_get(self.feature_id, field_id=1)
+      with client.context():
+        actual_response = self.handler.do_get(self.feature_id, field_id=1)
     self.assertEqual({"approvals": []}, actual_response)
 
   def test_get__field_some(self):
     """We can get approvals for a given feature and field_id."""
     testing_config.sign_out()
-    self.appr_1_1.put()
-    self.appr_1_2.put()
+    with client.context():
+      self.appr_1_1.put()
+      self.appr_1_2.put()
 
     with register.app.test_request_context(self.request_path + '/1'):
-      actual_response = self.handler.do_get(self.feature_id, field_id=1)
+      with client.context():
+        actual_response = self.handler.do_get(self.feature_id, field_id=1)
 
     self.assertEqual(
         {"approvals": [self.expected1]},
@@ -117,48 +128,56 @@ class ApprovalsAPITest(unittest.TestCase):
     params = {}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     params = {'featureId': 'not an int'}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
   def test_post__bad_field_id(self):
     """Handler rejects requests that don't specify a field ID correctly."""
     params = {'featureId': self.feature_id}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     params = {'featureId': self.feature_id, 'fieldId': 'not an int'}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     params = {'featureId': self.feature_id, 'fieldId': 999}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
   def test_post__bad_state(self):
     """Handler rejects requests that don't specify a state correctly."""
     params = {'featureId': self.feature_id, 'fieldId': 1}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     params = {'featureId': self.feature_id, 'fieldId': 1,
               'state': 'not an int'}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     params = {'featureId': self.feature_id, 'fieldId': 1,
               'state': 999}
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.BadRequest):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
   def test_post__feature_not_found(self):
     """Handler rejects requests that don't match an existing feature."""
@@ -166,7 +185,8 @@ class ApprovalsAPITest(unittest.TestCase):
               'state': models.Approval.NEED_INFO }
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.NotFound):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
   @mock.patch('internals.approval_defs.get_approvers')
   def test_post__forbidden(self, mock_get_approvers):
@@ -178,17 +198,20 @@ class ApprovalsAPITest(unittest.TestCase):
     testing_config.sign_out()
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.Forbidden):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     testing_config.sign_in('user7@example.com', 123567890)
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.Forbidden):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
     testing_config.sign_in('user@google.com', 123567890)
     with register.app.test_request_context(self.request_path, json=params):
       with self.assertRaises(werkzeug.exceptions.Forbidden):
-        self.handler.do_post()
+        with client.context():
+          self.handler.do_post()
 
   @mock.patch('internals.approval_defs.get_approvers')
   def test_post__add_or_update(self, mock_get_approvers):
@@ -198,10 +221,12 @@ class ApprovalsAPITest(unittest.TestCase):
     params = {'featureId': self.feature_id, 'fieldId': 1,
               'state': models.Approval.NEED_INFO}
     with register.app.test_request_context(self.request_path, json=params):
-      actual = self.handler.do_post()
+      with client.context():
+        actual = self.handler.do_post()
 
     self.assertEqual(actual, {'message': 'Done'})
-    updated_approvals = models.Approval.get_approvals(self.feature_id)
+    with client.context():
+      updated_approvals = models.Approval.get_approvals(self.feature_id)
     self.assertEqual(1, len(updated_approvals))
     appr = updated_approvals[0]
     self.assertEqual(appr.feature_id, self.feature_id)
