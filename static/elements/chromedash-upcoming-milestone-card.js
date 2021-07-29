b77f2604c3870df414b27bfc7cb4e7026e96b161
shivamag00 <shivam.agrawal.2000@gmail.com>
2021-07-29 17:47:13 +0530

    Made render method modular

--- static/elements/chromedash-upcoming-milestone-card.js
+++ static/elements/chromedash-upcoming-milestone-card.js
@@ -82,89 +82,108 @@ class ChromedashUpcomingMilestoneCard extends LitElement {
     return Object.keys(obj).sort();
   }
 
-  render() {
+  _cardHeaderTemplate() {
     return html `
-      <section class="release ${this.showBlink ? nothing : 'no-components'}">
-        <div class="layout vertical center">
-          <h1 class="channel_label">${this.templateContent.channelLabel}</h1>
-          <h1 class="chrome_version layout horizontal center ${this.templateContent.h1Class}">
-          <span class="chrome-logo"></span>
-          <a href="${this.templateContent.downloadUrl}" title="${this.templateContent.downloadTitle}"
-            target="_blank">Chrome ${this.channel.version}</a>
-          </h1>
-        </div>
-        ${this.showDates && this.channel.earliest_beta ? html`
-        <div class="milestone_info layout horizontal center-center">
-          <h3>
-            <span class="channel_label">Beta</span> ${this.templateContent.dateText}
-            <span class="milestone_info-beta">${this._computeDate(this.channel.earliest_beta)} - ${this._computeDate(this.channel.latest_beta)}</span>
-          </h3>
-        </div>
-        <div class="milestone_info layout horizontal center-center">
-          <h3>
-            <span class="channel_label">Stable</span> ${this._computeDaysUntil(this.channel.stable_date)}
-            <span class="release-stable">( ${this._computeDate(this.channel.stable_date)} )</span>
-          </h3>
-        </div>
-        ` : nothing}
-        <div class="features_list">
-          <div class="features_header">${this.templateContent.featureHeader}:</div>
+      <div class="layout vertical center">
+        <h1 class="channel_label">${this.templateContent.channelLabel}</h1>
+        <h1 class="chrome_version layout horizontal center ${this.templateContent.h1Class}">
+        <span class="chrome-logo"></span>
+        <a href="${this.templateContent.downloadUrl}" title="${this.templateContent.downloadTitle}"
+          target="_blank">Chrome ${this.channel.version}</a>
+        </h1>
+      </div>
+      ${this.showDates && this.channel.earliest_beta ? html`
+      <div class="milestone_info layout horizontal center-center">
+        <h3>
+          <span class="channel_label">Beta</span> ${this.templateContent.dateText}
+          <span class="milestone_info-beta">${this._computeDate(this.channel.earliest_beta)} - ${this._computeDate(this.channel.latest_beta)}</span>
+        </h3>
+      </div>
+      <div class="milestone_info layout horizontal center-center">
+        <h3>
+          <span class="channel_label">Stable</span> ${this._computeDaysUntil(this.channel.stable_date)}
+          <span class="release-stable">( ${this._computeDate(this.channel.stable_date)} )</span>
+        </h3>
+      </div>
+      ` : nothing}
+    `;
+  }
 
-            ${this._objKeys(this.channel.components).map((componentName) => html`
-            <h3 class="feature_components">${componentName}</h3>
-            <ul>
-              ${this.channel.components[componentName].map((f) => html`
-              <li data-feature-id="${f.id}">
-                <a href="/feature/${f.id}">${f.name}</a>
-                <span class="icon_row">
-                ${f.browsers.chrome.origintrial ? html`
-                  <span class="tooltip" title="Origin Trial">
-                    <iron-icon icon="chromestatus:extension" class="experimental" data-tooltip></iron-icon>
-                  </span>
-                  ` : nothing}
-                  ${f.browsers.chrome.intervention ? html`
-                  <span class="tooltip" title="Browser intervention">
-                    <iron-icon icon="chromestatus:pan-tool" class="intervention" data-tooltip></iron-icon>
-                  </span>
-                  ` : nothing}
-                  ${this.removedStatus.includes(f.browsers.chrome.status.text) ? html`
-                  <span class="tooltip" title="Removed">
-                    <iron-icon icon="chromestatus:cancel" class="remove" data-tooltip></iron-icon>
-                  </span>
-                  ` : nothing}
-                  ${this.deprecatedStatus.includes(f.browsers.chrome.status.text) ? html`
-                  <span class="tooltip" title="Deprecated">
-                    <iron-icon icon="chromestatus:warning" class="deprecated" data-tooltip></iron-icon>
-                  </span>
-                  ` : nothing}
-                  ${this.signedIn ? html`
-                  <span class="tooltip"
-                    title="Receive an email notification when there are updates">
-                    <iron-icon
-                      icon="${this.starredFeatures.includes(Number(f.id)) ?
-                      'chromestatus:star' :
-                      'chromestatus:star-border'}"
-                      class="pushicon"
-                      data-feature-id="${f.id}"
-                      @click="${this.toggleStar}">
-                    </iron-icon>
-                  </span>
-                  ` : html`
-                  <span class="tooltip"
-                    title="Sign in to get email notifications for updates">
-                    <a href="#"  @click="${window.promptSignIn}" data-tooltip>
-                      <iron-icon icon="chromestatus:star-border"
-                        class="pushicon">
-                      </iron-icon>
-                    </a>
-                  </span>
-                  `}
-                </span>
-              </li>
-              `)}
-            </ul>
-          `)}
-        </div>
+  _cardFeatureItemTemplate(f) {
+    return html `
+    <li data-feature-id="${f.id}">
+      <a href="/feature/${f.id}">${f.name}</a>
+      <span class="icon_row">
+        ${f.browsers.chrome.origintrial ? html`
+        <span class="tooltip" title="Origin Trial">
+          <iron-icon icon="chromestatus:extension" class="experimental" data-tooltip></iron-icon>
+        </span>
+        ` : nothing}
+        ${f.browsers.chrome.intervention ? html`
+        <span class="tooltip" title="Browser intervention">
+          <iron-icon icon="chromestatus:pan-tool" class="intervention" data-tooltip></iron-icon>
+        </span>
+        ` : nothing}
+        ${this.removedStatus.includes(f.browsers.chrome.status.text) ? html`
+        <span class="tooltip" title="Removed">
+          <iron-icon icon="chromestatus:cancel" class="remove" data-tooltip></iron-icon>
+        </span>
+        ` : nothing}
+        ${this.deprecatedStatus.includes(f.browsers.chrome.status.text) ? html`
+        <span class="tooltip" title="Deprecated">
+          <iron-icon icon="chromestatus:warning" class="deprecated" data-tooltip></iron-icon>
+        </span>
+        ` : nothing}
+        ${this.signedIn ? html`
+        <span class="tooltip"
+          title="Receive an email notification when there are updates">
+          <iron-icon
+            icon="${this.starredFeatures.includes(Number(f.id)) ?
+            'chromestatus:star' :
+            'chromestatus:star-border'}"
+            class="pushicon"
+            data-feature-id="${f.id}"
+            @click="${this.toggleStar}">
+          </iron-icon>
+        </span>
+        ` : html`
+        <span class="tooltip"
+          title="Sign in to get email notifications for updates">
+          <a href="#"  @click="${window.promptSignIn}" data-tooltip>
+            <iron-icon icon="chromestatus:star-border"
+              class="pushicon">
+            </iron-icon>
+          </a>
+        </span>
+        `}
+      </span>
+    </li>
+    
+    `;
+  }
+
+  _cardFeatureListTemplate() {
+    return html `
+      <div class="features_list">
+        <div class="features_header">${this.templateContent.featureHeader}:</div>
+
+          ${this._objKeys(this.channel.components).map((componentName) => html`
+          <h3 class="feature_components">${componentName}</h3>
+          <ul>
+            ${this.channel.components[componentName].map((f) => html`
+              ${this._cardFeatureItemTemplate(f)}
+            `)}
+          </ul>
+        `)}
+      </div>
+    `;
+  }
+
+  render() {
+    return html`
+      <section class="release ${this.showBlink ? nothing : 'no-components'}">
+        ${this._cardHeaderTemplate()}
+        ${this._cardFeatureListTemplate()}    
       </section>
     `;
   }
