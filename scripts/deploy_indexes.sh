1286dc308c15ae7c3622815b400e58f8c8b48ce4
Jason Robbins <jrobbins@gmail.com>
2021-08-19 17:43:21 -0700

    Add indexes needed for /upcoming page (#1478)
    * Add indexes needed for /upcoming page.
    
    * Added deploy_indexes.sh

---
+++ scripts/deploy_indexes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Deploys the datastore indexes to App Engine.
+#
+# Note: This script should be used in place of using appcfg.py update directly
+# to update the application on App Engine.
+
+
+appName=${1:-cr-status}
+usage="Usage: deploy_indexes.sh cr-status-staging"
+
+# The directory in which this script resides.
+readonly BASEDIR=$(dirname $BASH_SOURCE)
+
+$BASEDIR/oauthtoken.sh deploy
+gcloud app deploy \
+  --project $appName \
+  $BASEDIR/../index.yaml
