1faef5482b3060e7e6db7f068125531155570924
Jason Robbins <jrobbins@gmail.com>
2021-09-30 11:03:44 -0700

    Detect intent emails to create pending approval entities. (#1567)
    * Detect intent emails to create pending approval entities.
    
    * Separate experiment from extend-experiment and fix bug with "Re:"

--- internals/detect_intent.py
+++ internals/detect_intent.py
@@ -13,13 +13,77 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import division
-from __future__ import print_function
-
+import re
 import logging
 
 import settings
 from framework import basehandlers
+from internals import approval_defs
+from internals import models
+
+
+def detect_field(subject):
+  """Look for key words in the subject line that indicate intent type.
+
+  These should detect recent actual intent threads as seen in the blink-dev
+  archive: https://groups.google.com/a/chromium.org/g/blink-dev.
+  """
+  subject = subject.lower().strip()
+  if subject.startswith('[blink-dev]'):
+    subject = subject[len('[blink-dev]'):].strip()
+  while subject.startswith('re:'):
+    subject = subject[len('re:'):].strip()
+
+  if (subject.startswith('intent to ship') or
+      subject.startswith('intent to prototype and ship') or
+      subject.startswith('intent to implement and ship') or
+      subject.startswith('intent to deprecate and remove') or
+      subject.startswith('intent to remove')):
+    return approval_defs.ShipApproval
+
+  if (subject.startswith('intent to prototype') or
+      subject.startswith('intent to implement') or
+      subject.startswith('intent to deprecate')):
+    return approval_defs.PrototypeApproval
+
+  if (subject.startswith('intent to experiment') or
+      subject.startswith('request for deprecation trial')):
+    return approval_defs.ExperimentApproval
+
+  if (subject.startswith('intent to continue experiment') or
+      subject.startswith('intent to extend experiment') or
+      subject.startswith('intent to continue origin') or
+      subject.startswith('intent to extend origin')):
+    return approval_defs.ExtendExperimentApproval
+
+  return None
+
+
+CHROMESTATUS_LINK_RE = re.compile(
+    r'Link to entry on the Chrome Platform Status:?\s+'
+    r'https://www.chromestatus.com/feature/(\d+)', re.I)
+
+
+def detect_feature_id(body):
+  """Look for the link to a chromestatus entry."""
+  match = CHROMESTATUS_LINK_RE.search(body)
+  if match:
+    return int(match.group(1))
+  return None
+
+
+THREAD_LINK_RE = re.compile(
+    r'To view this discussion on the web visit\s+'
+    r'(https://groups.google.com/a/chromium.org/d/msgid/blink-dev/'
+    r'\S+)[.]')
+
+
+def detect_thread_url(body):
+  """Look for the link to the thread in the blink-dev archive."""
+  match = THREAD_LINK_RE.search(body)
+  if match:
+    return match.group(1)
+  return None
 
 
 class IntentEmailHandler(basehandlers.FlaskHandler):
@@ -42,12 +106,59 @@ class IntentEmailHandler(basehandlers.FlaskHandler):
                  'Body:        %r\n',
                  from_addr, subject, in_reply_to, body)
 
-    # TODO(jrobbins): Write code to parse out:
-    # 1. The type of intent.
-    # 2. The feature ID number
-    # 3. Any clear "LGTM"s
-    # And retrieve model objects for the feature and the sending user.
-    # Set the thread URL on the feature.
-    # Set an LGTM if appropriate.
+    approval_field = detect_field(subject)
+    if not approval_field:
+      logging.info('This does not appear to be an intent')
+      return {'message': 'Not an intent'}
 
+    feature_id = detect_feature_id(body)
+    if not feature_id:
+      logging.info('Could not find feature ID')
+      return {'message': 'No feature ID'}
+    feature = models.Feature.get_by_id(feature_id)
+    if not feature:
+      logging.info('Could not retrieve feature')
+      return {'message': 'Feature not found'}
+
+    thread_url = detect_thread_url(body)
+
+    self.set_intent_thread_url(feature, approval_field, thread_url)
+    self.create_approvals(feature_id, approval_field, from_addr, body)
     return {'message': 'Done'}
+
+  def set_intent_thread_url(self, feature, approval_field, thread_url):
+    """If the feature has no previous thread URL for this intent, set it."""
+    if not thread_url:
+      return
+
+    if (approval_field == approval_defs.PrototypeApproval and
+        not feature.intent_to_implement_url):
+      feature.intent_to_implement_url = thread_url
+      feature.put()
+
+    # TODO(jrobbins): Ready-for-trial threads
+
+    if (approval_field == approval_defs.ExperimentApproval and
+        not feature.intent_to_experiment_url):
+      feature.intent_to_experiment_url = thread_url
+      feature.put()
+
+    if (approval_field == approval_defs.ShipApproval and
+        not feature.intent_to_ship_url):
+      feature.intent_to_ship_url = thread_url
+      feature.put()
+
+  def create_approvals(self, feature_id, approval_field, from_addr, body):
+    """Store either a NEEDS_REVIEW or an APPROVED approval value."""
+    # Case 1: This is a new intent thread
+    existing_approvals = models.Approval.get_approvals(
+        feature_id=feature_id, field_id=approval_field.field_id)
+    if not existing_approvals:
+      models.Approval.set_approval(
+          feature_id, approval_field.field_id,
+          models.Approval.NEEDS_REVIEW, from_addr)
+
+    # Case 2: This is an existing intent thread
+    # TODO(jrobbins): Detect LGTMs in body, verify that sender has permission,
+    # set an approval value, and clear the original NEEDS_REVIEW if
+    # the approval rule (1 or 3 LTGMs) is satisfied.
