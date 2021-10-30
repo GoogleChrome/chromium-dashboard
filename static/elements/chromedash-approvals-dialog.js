d76511bdb8fe17e6fb00c8fcc237748439402162
Jason Robbins <jrobbins@gmail.com>
2021-10-30 19:01:30 -0400

    progress

--- static/elements/chromedash-approvals-dialog.js
+++ static/elements/chromedash-approvals-dialog.js
@@ -4,7 +4,6 @@ import './chromedash-dialog';
 import SHARED_STYLES from '../css/shared.css';
 
 const STATE_NAMES = [
-  [-1, 'No value'],
   [0, 'Needs review'],
   [1, 'N/a or Ack'],
   [3, 'Review started'],
@@ -44,6 +43,8 @@ class ChromedashApprovalsDialog extends LitElement {
       comments: {type: Array},
       canApprove: {type: Boolean},
       showAllIntents: {type: Boolean},
+      changedApprovalsByField: {type: Map},
+      needsSave: {type: Boolean},
     };
   }
 
@@ -57,6 +58,8 @@ class ChromedashApprovalsDialog extends LitElement {
     this.canApprove = false;
     this.subsetPending = false;
     this.showAllIntents = false;
+    this.changedApprovalsByField = new Map();
+    this.needsSave = false;
   }
 
   connectedCallback() {
@@ -76,7 +79,7 @@ class ChromedashApprovalsDialog extends LitElement {
         }
 
         .approval_section div {
-          margin-left: var(--content-padding-half);
+          margin-left: var(--content-padding);
         }
 
         .approval_row {
@@ -101,6 +104,9 @@ class ChromedashApprovalsDialog extends LitElement {
          float: left;
         }
 
+        textarea {
+          padding: 4px;
+        }
       `];
   }
 
@@ -118,6 +124,8 @@ class ChromedashApprovalsDialog extends LitElement {
         this.subsetPending = (numPending > 0 &&
                               numPending < APPROVAL_DEFS.length);
         this.showAllIntents = numPending == 0;
+        this.changedApprovalsByField = new Map();
+        this.needsSave = false;
       });
     const p3 = window.csClient.getComments(this.featureId).then(
       (res) => {
@@ -142,25 +150,47 @@ class ChromedashApprovalsDialog extends LitElement {
   }
 
   renderApprovalValue(approvalValue) {
+    const selectedValue = (
+      this.changedApprovalsByField.get(approvalValue.field_id) ||
+          approvalValue.state);
+    let placeholderOption = nothing;
+    if (approvalValue.state == -1) {
+      placeholderOption = html`
+        <option value="-1" selected>No value</option>
+      `;
+    }
+
     return html`
       <div class="approval_row">
         <span class="set_by">${approvalValue.set_by}</span>
         ${approvalValue.set_by == this.signedInUser ? html`
-          <select value=${approvalValue.state}>
+        <select
+          selected=${selectedValue}
+          data-field="${approvalValue.field_id}"
+          @change=${this.handleSelectChanged}
+        >
+            ${placeholderOption}
             ${STATE_NAMES.map((valName) => html`
-              <option value="${valName[0]}">${valName[1]}</option>`
+              <option value="${valName[0]}"
+                ?selected=${valName[0] == selectedValue}
+               >${valName[1]}</option>`
               )}
           </select>` : html`
-          ${STATE_NAMES[approvalValue.state + 1][1]}
+          ${STATE_NAMES[approvalValue.state][1]}
           `}
        </div>
     `;
   }
 
-  renderAddApproval() {
-    // conditional
-    return this.renderApprovalValue(
-      {set_by: this.signedInUser, value: -1});
+  renderAddApproval(fieldId) {
+    const existingApprovalByMe = this.approvals.some((a) =>
+      a.field_id == fieldId);
+    if (existingApprovalByMe) {
+      return nothing;
+    } else {
+      return this.renderApprovalValue(
+        {set_by: this.signedInUser, state: -1, field_id: fieldId});
+    }
   }
 
   renderApproval(approvalDef) {
@@ -185,7 +215,7 @@ class ChromedashApprovalsDialog extends LitElement {
       <div class="approval_section">
         <h3>${approvalDef.name}</h3>
         ${approvalValues.map((av) => this.renderApprovalValue(av))}
-        ${this.renderAddApproval()}
+        ${this.renderAddApproval(approvalDef.id)}
         ${threadLink}
       </div>
     `;
@@ -212,6 +242,12 @@ class ChromedashApprovalsDialog extends LitElement {
     `;
   }
 
+  renderAllComments() {
+    return html`
+        <h3>Comments</h3>
+        ${this.comments.map(this.renderComment)}
+    `;
+  }
 
   renderControls() {
     let showAllCheckbox = nothing;
@@ -225,16 +261,21 @@ class ChromedashApprovalsDialog extends LitElement {
     }
 
     return html`
-     <div style="margin-top:2em">
-      <textarea rows=4 cols=80></textarea>
+     <div>
+      <textarea rows=4 cols=80 placeholder="Add a comment"></textarea>
      </div>
      <div class="controls">
        ${showAllCheckbox}
          <label id="post_link_dev" style="margin-right:1em"><input
           type="checkbox"
           >Post to blink-dev</label>
-       <button class="primary">Save</button>
-       <button>Cancel</button>
+       <button class="primary"
+          @click=${this.handleSave}
+          ?disabled=${!this.needsSave}
+          >Save</button>
+       <button
+          @click=${this.handleCancel}
+          >Cancel</button>
      </div>
     `;
   }
@@ -245,10 +286,40 @@ class ChromedashApprovalsDialog extends LitElement {
       <chromedash-dialog name="${dialogName}">
         ${this.renderLoading()}
         ${this.renderAllApprovals()}
-        ${this.comments.map(this.renderComment)}
+        ${this.renderAllComments()}
         ${this.renderControls()}
       </chromedash-dialog>
     `;
+  }
+
+  handleSelectChanged(e) {
+    console.log('In handleSelectChanged()');
+    const fieldId = e.target.dataset['field'];
+    const newVal = e.target.value;
+    this.changedApprovalsByField.set(fieldId, newVal);
+    this.needsSave = true;
+    console.log(this.changedApprovalsByField);
+    console.log(this.needsSave);
+  }
+
+  handleSave() {
+    console.log('In save()');
+    const promises = [];
+    for (let fieldId of this.changedApprovalsByField.keys()) {
+      if (this.changedApprovalsByField.get(fieldId) != -1) {
+        promises.push(
+          window.csClient.setApproval(
+            this.feature.id, fieldId,
+            this.changedApprovalsByField.get(fieldId)));
+      }
+    }
+    Promise.all(promises).then(() => {
+      this.shadowRoot.querySelector('chromedash-dialog').close();
+    });
+  }
+
+  handleCancel() {
+    this.shadowRoot.querySelector('chromedash-dialog').close();
   }
 }
 
