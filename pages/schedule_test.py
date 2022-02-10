32cff34cb9d03262d62e9cff37c2602a36b57312
Daniel LaLiberte <dlaliberte@google.com>
2022-02-10 19:07:45 +0000

    Add html validation to more tests

--- pages/schedule_test.py
+++ pages/schedule_test.py
@@ -13,10 +13,52 @@
 # limitations under the License.
 
 import testing_config  # Must be imported first
+
+import os
+import flask
+
 from unittest import mock
 import unittest
+import html5lib
 
+from framework import ramcache
 from pages import schedule
+
+test_app = flask.Flask(__name__)
+
+class TestWithFeature(testing_config.CustomTestCase):
+
+  REQUEST_PATH_FORMAT = 'subclasses fill this in'
+  HANDLER_CLASS = 'subclasses fill this in'
+
+  def setUp(self):
+    self.request_path = self.REQUEST_PATH_FORMAT
+    self.handler = self.HANDLER_CLASS()
+
+  def tearDown(self):
+    ramcache.flush_all()
+    ramcache.check_for_distributed_invalidation()
+
+class ScheduleTemplateTest(TestWithFeature):
+
+  HANDLER_CLASS = schedule.ScheduleHandler
+
+  def setUp(self):
+    super(ScheduleTemplateTest, self).setUp()
+    with test_app.test_request_context(self.request_path):
+      self.template_data = self.handler.get_template_data()
+
+      self.template_data.update(self.handler.get_common_data())
+      self.template_data['nonce'] = 'fake nonce'
+      template_path = self.handler.get_template_path(self.template_data)
+      self.full_template_path = os.path.join(template_path)
+
+  def test_html_rendering(self):
+    """We can render the template with valid html."""
+    template_text = self.handler.render(
+        self.template_data, self.full_template_path)
+    parser = html5lib.HTMLParser(strict=True)
+    document = parser.parse(template_text)
 
 
 class ScheduleFunctionsTest(unittest.TestCase):
