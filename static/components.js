54dac28ba3e0d76d6b376f3a277d34c637587de0
Jason Robbins <jrobbins@gmail.com>
2022-02-23 19:27:21 -0500

    Reduce unneeded polymer deps.

--- static/components.js
+++ static/components.js
@@ -1,15 +1,10 @@
 /** This is the entry file for rollup. It bundles all the web components: polymer-paper components and our own components */
 
 // polymer components
-import '@polymer/app-layout';
-import '@polymer/app-layout/app-scroll-effects/effects/waterfall';
 import '@polymer/iron-collapse';
 import '@polymer/iron-icon';
 import '@polymer/iron-iconset-svg';
-import '@polymer/paper-item';
-import '@polymer/paper-listbox';
-import '@polymer/paper-ripple';
-import '@polymer/paper-styles/color.js';
+
 
 // chromedash components
 import './elements/icons';
