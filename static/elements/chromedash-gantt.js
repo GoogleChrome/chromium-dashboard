a81256a3dbdd2dcefebd5bd687ab7239da2a06c5
Jason Robbins <jrobbins@gmail.com>
2022-04-15 02:24:43 +0000

    Redo gantt charts to clarify gaps.

--- static/elements/chromedash-gantt.js
+++ static/elements/chromedash-gantt.js
@@ -23,50 +23,75 @@ class ChromedashGantt extends LitElement {
       css`
       :host {
         width: 600px;
-        margin-top: 8px;
       }
+
+      label {
+        display: block;
+        font-weight: 500;
+        margin-right: 5px;
+        padding-top: var(--content-padding);
+      }
+
+      .platform-row {
+        margin: var(--content-padding) 0;
+      }
+
       .platform {
         display: inline-block;
-        padding-top: 15px;
+        padding-top: 7px;
         vertical-align: top;
         width: 100px;
       }
-      .chart {
-        display: inline-block;
+
+      /* On small displays, show milestones as a bullet list. */
+      .chart li {
+        list-style: circle;
+        margin-left: var(--content-padding);
       }
 
-
-      .strip div {
-        display: inline-block;
-        width: 50px;
-        height: 30px;
-        overflow: visible;
-        white-space: nowrap;
-        background: #eee;
-        padding: 4px;
-        line-height: 22px;
-        margin: 3px 2px;
+      .empty {
+        display: none;
       }
 
-      .strip div.dev_trial {
-        background: #cfe2f3ff;
+      /* On large displays, show milestones as a gantt chart. */
+      @media only screen and (min-width: 701px) {
+        .chart {
+          display: inline-grid;
+          grid-auto-columns: 50px;
+          grid-auto-rows: 30px;
+          gap: 2px;
+        }
+
+        .chart li {
+          list-style: none;
+          margin-left: 0;
+          height: 30px;
+          overflow: visible;
+          white-space: nowrap;
+          padding: 4px;
+          line-height: 22px;
+        }
+
+        .empty {
+          display: block;
+          background: #eee;
+        }
+
+        .chart .dev_trial {
+          background: #cfe2f3ff;
+        }
+
+        .chart .origin_trial {
+          background: #6fa8dcff;
+        }
+
+        .chart .shipping {
+          background: #0b5394ff;
+          color: white;
+        }
       }
-      .strip div.origin_trial {
-        background: #6fa8dcff;
-      }
-      .strip div.shipping {
-        background: #0b5394ff;
-        color: white;
-      }
+
     `];
-  }
-
-  firstPhrase(milestone) {
-    if (milestone <= this.stableMilestone) {
-      return 'First milestone with this feature';
-    } else {
-      return 'First expected milestone with this feature';
-    }
   }
 
   _isInactive() {
@@ -76,6 +101,34 @@ class ChromedashGantt extends LitElement {
             status === 'No longer pursuing');
   }
 
+  renderChartRow(gridRow, first, last, sortedMilestones, cssClass, label) {
+    const cellsOnRow = [];
+    for (let col = 0; col < sortedMilestones.length; col++) {
+      const m = sortedMilestones[col];
+      if (m < first || m > last) {
+        cellsOnRow.push(html`
+          <li style="grid-row: ${gridRow};
+                     grid-column: ${col + 1}"
+              class="empty"></li>
+        `);
+      }
+    }
+
+    const firstCol = sortedMilestones.indexOf(first);
+    const lastCol = sortedMilestones.indexOf(last);
+    const span = Math.max(0, lastCol - firstCol) + 1;
+    cellsOnRow.push(html`
+      <li style="grid-row: ${gridRow};
+                 grid-column: ${firstCol + 1} / span ${span}"
+          class="${cssClass}">
+        ${label}
+      </li>
+    `);
+
+    return cellsOnRow;
+  }
+
+
   renderPlatform(
     platform, devTrialMilestone, originTrialMilestoneFirst,
     originTrialMilestoneLast, shippingMilestone,
@@ -83,30 +136,53 @@ class ChromedashGantt extends LitElement {
     if (!devTrialMilestone && !originTrialMilestoneFirst && !shippingMilestone) {
       return nothing;
     }
+    const maxMilestone = Math.max(...sortedMilestones);
 
-    const dtOffset = sortedMilestones.dt.indexOf(devTrialMilestone);
-    const otOffset = sortedMilestones.ot.indexOf(originTrialMilestoneFirst);
-    const shipOffset = sortedMilestones.ship.indexOf(shippingMilestone);
+    let gridRow = 1;
+
+    let dtChartRow = nothing;
+    if (devTrialMilestone) {
+      let devTrialMilestoneLast = maxMilestone;
+      // If there is a shipping milestone, Dev trial stops just before it.
+      if (shippingMilestone) {
+        const shippingIndex = sortedMilestones.indexOf(shippingMilestone);
+        devTrialMilestoneLast = sortedMilestones[shippingIndex - 1];
+      }
+      dtChartRow = this.renderChartRow(
+        gridRow, devTrialMilestone, devTrialMilestoneLast,
+        sortedMilestones, 'dev_trial',
+        'Dev Trial: ' + devTrialMilestone);
+      gridRow++;
+    }
+
+    let otChartRow = nothing;
+    if (originTrialMilestoneFirst) {
+      otChartRow = this.renderChartRow(
+        gridRow, originTrialMilestoneFirst, originTrialMilestoneLast,
+        sortedMilestones, 'origin_trial',
+        'Origin Trial: ' + originTrialMilestoneFirst +
+          ' to ' + originTrialMilestoneLast);
+      gridRow++;
+    }
+
+    let shipChartRow = nothing;
+    if (shippingMilestone) {
+      shipChartRow = this.renderChartRow(
+        gridRow, shippingMilestone, maxMilestone,
+        sortedMilestones, 'shipping',
+        'Shipping: ' + shippingMilestone);
+      gridRow++;
+    }
 
     return html`
-       <li><div class="platform">${platform}</div>
+       <li class="platform-row">
+         <div class="platform">${platform}</div>
 
-       <div class="chart">
-           <div class="strip">
-             <div>97</div><div>98</div
-             ><div style="width: ${7*50 + 4*6}px" class="dev_trial">${devTrialMilestone}  ${dtOffset}</div
-             ><div>106</div><div>107</div>
-           </div>
-           <div class="strip">
-             <div>97</div><div>98</div><div>99</div><div>100</div
-             ><div style="width: ${4*50 + 4*3}px" class="origin_trial">${originTrialMilestoneFirst}  ${otOffset}</div
-             ><div>105</div><div>106</div><div>107</div>
-           </div>
-           <div class="strip">
-             <div>97</div><div>98</div><div>99</div><div>100</div><div>101</div><div>102</div><div>103</div><div>104</div><div>105</div
-             ><div style="width: ${2*50 + 4*1}px" class="shipping">${shippingMilestone} ${shipOffset}</div>
-           </div>
-         </div>
+         <ul class="chart">
+            ${dtChartRow}
+            ${otChartRow}
+            ${shipChartRow}
+         </ul>
        </li>
     `;
   }
@@ -122,18 +198,13 @@ class ChromedashGantt extends LitElement {
     const otAndroidLast = f.ot_milestone_android_end;
     const shipAndroid = f.browsers.chrome.android;
     const dtIos = f.dt_milestone_ios_start;
-    const otIos = null; // Chrome on iOS does not support OT.
+    const otIosFirst = null; // Chrome on iOS does not support OT.
+    const otIosLast = null; // Chrome on iOS does not support OT.
     const shipIos = f.browsers.chrome.ios;
     const dtWebview = f.dt_milestone_webview_start;
-    const otWebview = null; // Webview does not support OT.
+    const otWebviewFirst = null; // Webview does not support OT.
+    const otWebviewLast = null; // Webview does not support OT.
     const shipWebview = f.browsers.chrome.webview;
-
-    if (!dtDesktop && !otDesktopFirst && !shipDesktop &&
-        !dtAndroid && !otAndroidFirst && !shipAndroid &&
-        !dtIos && !otIos && !shipIos &&
-        !dtWebview && !otWebview && !shipWebview) {
-      return html`<p>No milestones specified</p>`;
-    }
 
     // Don't show the visualization if there is no active development.
     // But, any milestones are available as text in the details section.
@@ -141,14 +212,57 @@ class ChromedashGantt extends LitElement {
       return nothing;
     }
 
-    const sortedMilestones = {
-      dt: [dtDesktop, dtAndroid, dtIos, dtWebview].sort(),
-      ot: [otDesktopFirst, otAndroidFirst, otIos, otWebview].sort(),
-      ship: [shipDesktop, shipAndroid, shipIos, shipWebview].sort(),
-    };
+    const allMilestones = [
+      dtDesktop, dtAndroid, dtIos, dtWebview,
+      otDesktopFirst, otDesktopLast,
+      otAndroidFirst, otAndroidLast,
+      otIosFirst, otIosLast,
+      otWebviewFirst, otWebviewLast,
+      shipDesktop, shipAndroid, shipIos, shipWebview].filter(x => x);
+
+    if (allMilestones.length == 0) {
+      return html`<p>No milestones specified</p>`;
+    }
+
+    const minMilestone = Math.min(...allMilestones);
+    const maxMilestone = Math.max(...allMilestones);
+    // We always show one extra after the last milestone so that the
+    // "Shipped" block has room for that text.
+    const milestoneRange = (maxMilestone - minMilestone + 1) + 1;
+    // sortedMilestones would be the list of column heading labels,
+    // execpt that they are not shown.
+    let sortedMilestones;
+
+    if (milestoneRange <= 12) {
+      // First choice:
+      // Use columns for every milestone in the range min...max.
+      // In python it would be range(minMilestone, maxMilestone + 1)
+      sortedMilestones = Array(milestoneRange).fill(minMilestone).map(
+        (x, y) => x + y);
+    } else {
+      // Second choice:
+      // Use columns for each milestone value and the one after it
+      // even if that means that milestone numbers are not consecutive.
+      const augmentedMilestoneSet = new Set(allMilestones);
+      for (const m of allMilestones) {
+        augmentedMilestoneSet.add(m + 1);
+      }
+      sortedMilestones = Array.from(augmentedMilestoneSet).sort(
+        (a, b) => a - b);
+
+      if (sortedMilestones.length > 12) {
+        // Third choice:
+        // Use columns for exactly those milestones that are actually used
+        // even if that means that milestone numbers are not consecutive.
+        const milestoneSet = new Set(allMilestones);
+        sortedMilestones = Array.from(milestoneSet).sort(
+          (a, b) => a - b);
+        sortedMilestones.push(maxMilestone + 1); // After Shipped.
+      }
+    }
 
     return html`
-       <p>Estimated milestones:</p>
+       <label>Estimated milestones:</label>
        <ul>
        ${this.renderPlatform('Desktop',
           dtDesktop, otDesktopFirst, otDesktopLast, shipDesktop,
@@ -157,10 +271,10 @@ class ChromedashGantt extends LitElement {
           dtAndroid, otAndroidFirst, otAndroidLast, shipAndroid,
           sortedMilestones)}
        ${this.renderPlatform('iOS',
-          dtIos, otIos, otIos, shipIos,
+          dtIos, otIosFirst, otIosLast, shipIos,
           sortedMilestones)}
        ${this.renderPlatform('Webview',
-          dtWebview, otWebview, otWebview, shipWebview,
+          dtWebview, otWebviewFirst, otWebviewLast, shipWebview,
           sortedMilestones)}
        </ul>
     `;
