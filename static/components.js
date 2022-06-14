391b15e04df452abfcd719a644277d314266b01c
Daniel LaLiberte <dlaliberte@google.com>
2022-06-14 19:56:10 +0000

    Replace select and option with sl-select and sl-menu-item

--- static/components.js
+++ static/components.js
@@ -11,12 +11,15 @@ import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/details/details.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
+import '@shoelace-style/shoelace/dist/components/select/select.js';
 import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 
 // Set the base path to the folder you copied Shoelace's assets to
-setBasePath('@shoelace-style/shoelace'); // /dist/assets
+setBasePath('/static/shoelace');
 
 // chromedash components
 import './elements/icons';
