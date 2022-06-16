39527b01f81941cd405c9598d1f8b51f48b99b1a
Daniel LaLiberte <dlaliberte@google.com>
2022-06-16 18:26:14 +0000

    Add TODO about removing the subclass after shoelace deploys the fix.

--- static/elements/chromedash-select.js
+++ static/elements/chromedash-select.js
@@ -1,3 +1,5 @@
+// TODO Remove this once the shoelace fix is part of a new shoelace version.
+// See https://github.com/shoelace-style/shoelace/commit/624a8bbe71e59ae6255730e34d71e0b13d5ecf90
 
 import SlSelect from '@shoelace-style/shoelace/dist/components/select/select.js';
 
