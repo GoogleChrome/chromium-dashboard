539534bce77e278c00b07d33bdda6841e5c5206f
Daniel LaLiberte <dlaliberte@google.com>
2022-06-21 20:29:02 -0400

    Revert "Add chromedash-select to fix sl-select bug (#1944)" (#1963)
    This reverts commit a5c33851c552a9b4468db99391ec0d27aa6c6728.

--- static/components.js
+++ static/components.js
@@ -39,7 +39,6 @@ import './elements/chromedash-metadata';
 import './elements/chromedash-metrics';
 import './elements/chromedash-myfeatures';
 import './elements/chromedash-process-overview';
-import './elements/chromedash-select';
 import './elements/chromedash-textarea';
 import './elements/chromedash-timeline';
 import './elements/chromedash-toast';
