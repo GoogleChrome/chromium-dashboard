c4e9bcb940bfa6233c92c625033a45b109945f22
kevinshen56714 <kshen64@gatech.edu>
2022-07-05 18:38:06 -0400

    Move footer styles into its component

--- static/elements/chromedash-footer.js
+++ static/elements/chromedash-footer.js
@@ -1,11 +1,42 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 
+import {SHARED_STYLES} from '../sass/shared-css.js';
 
 export class ChromedashFooter extends LitElement {
+  static get styles() {
+    return [
+      ...SHARED_STYLES,
+      css`
+        footer {
+          background: var(--page-background);
+          box-shadow: 0 -2px 5px var(--shadow-color);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+          align-items: center;
+          margin-top: 2em;
+          padding: var(--content-padding-half);
+        }
+
+        footer div > * + * {
+          margin-left: var(--content-padding);
+          padding-left: var(--content-padding);
+        }
+
+        @media only screen and (min-width: 601px) and (min-height: 601px) {
+          footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+          }
+        } 
+    `];
+  }
+
   render() {
-    // TODO: Create a precomiled main css file and import it instead of inlining it here
     return html`
-      <link rel="stylesheet" href="/static/css/main.css">
       <footer>
         <div>
           <a href="https://github.com/GoogleChrome/chromium-dashboard/wiki/"
