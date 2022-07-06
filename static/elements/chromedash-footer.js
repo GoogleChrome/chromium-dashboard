0597cfd6a88e9de6cea408bdd056d8920e615014
Kuan-Hsuan (Kevin) Shen <kshen64@gatech.edu>
2022-07-06 11:56:19 -0400

    Convert footer into a component (#1997)
    * Convert footer into a component
    
    * Add footer unit test
    
    * Move footer styles into its component
    
    * Add footer-spacer

---
+++ static/elements/chromedash-footer.js
@@ -0,0 +1,66 @@
+import {LitElement, html, css} from 'lit';
+
+import {SHARED_STYLES} from '../sass/shared-css.js';
+
+export class ChromedashFooter extends LitElement {
+  static get styles() {
+    return [
+      ...SHARED_STYLES,
+      css`
+        footer {
+          background: var(--page-background);
+          box-shadow: 0 -2px 5px var(--shadow-color);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+          align-items: center;
+          margin-top: 2em;
+          padding: var(--content-padding-half);
+        }
+
+        footer div > * + * {
+          margin-left: var(--content-padding);
+          padding-left: var(--content-padding);
+        }
+
+        #footer-spacer {
+          display: none;
+          height: calc(54px + var(--content-padding) * 2);
+        }
+
+        @media only screen and (min-width: 601px) and (min-height: 601px) {
+          footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+          }
+
+          #footer-spacer {
+            display: block;
+          }
+        } 
+    `];
+  }
+
+  render() {
+    return html`
+      <div id="footer-spacer"></div>
+      <footer>
+        <div>
+          <a href="https://github.com/GoogleChrome/chromium-dashboard/wiki/"
+            target="_blank" rel="noopener">Help</a>
+          <a href="https://groups.google.com/a/chromium.org/forum/#!newtopic/blink-dev"
+            target="_blank" rel="noopener">Discuss</a>
+          <a href="https://github.com/GoogleChrome/chromium-dashboard/issues"
+            target="_blank" rel="noopener">File an issue</a>
+          <a href="https://policies.google.com/privacy"
+            target="_blank" rel="noopener">Privacy</a>
+        </div>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('chromedash-footer', ChromedashFooter);
