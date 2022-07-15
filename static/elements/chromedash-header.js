badbc97dad7c87e81444d77e188347a66fb96b87
Jason Robbins <jrobbins@gmail.com>
2022-07-15 09:57:12 -0700

    Move the new feature button to the heading. (#2022)
    * Move the new feature button to the heading.
    
    * remove unusedmedia query

--- static/elements/chromedash-header.js
+++ static/elements/chromedash-header.js
@@ -261,6 +261,11 @@ export class ChromedashHeader extends LitElement {
     return html`
       <div class="flex-container flex-container-inner-second">
       ${this.user ? html`
+        ${this.user.can_create_feature && !this.isCurrentPage('/guide/new') ? html`
+          <sl-button href="/guide/new" variant="primary" size="small">
+            Add feature
+          </sl-button>
+        `: nothing }
         <div class="nav-dropdown-container">
           <a class="nav-dropdown-trigger">
             ${this.user.email}
