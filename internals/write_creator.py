b40cd8476ace8987bccec7a2d388053736481968
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-07-18 11:58:19 -0700

    add logging

--- internals/write_creator.py
+++ internals/write_creator.py
@@ -1,3 +1,5 @@
+import logging
+
 from framework.basehandlers import FlaskHandler
 from internals.models import Feature
 
@@ -8,10 +10,15 @@ class UpdateCreatorHandler(FlaskHandler):
     """Writes string creator field from created_by user field."""
     q = Feature.query()
     features = q.fetch()
+    update_count = 0
     for feature in features:
       if feature.created_by and not feature.creator:
+        update_count += 1
         email = "Unknown"
         if feature.created_by:
             email = feature.created_by.email()
         feature.creator = email
         feature.put(notify=False)
+    
+    logging.info(f'{update_count} features updated with new creator field.')
+    return 'Success'
