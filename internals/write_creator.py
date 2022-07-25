e00530d2410446bfe42fb676102ec78fa9af5c23
Jason Robbins <jrobbins@gmail.com>
2022-07-25 22:28:54 +0000

    Add logging so that we can see partial progress

--- internals/write_creator.py
+++ internals/write_creator.py
@@ -12,12 +12,14 @@ class UpdateCreatorHandler(FlaskHandler):
     features = q.fetch()
     update_count = 0
     for feature in features:
+      logging.info(f'considering {feature.name}.')
       if feature.created_by and not feature.creator:
         update_count += 1
         email = "Unknown"
         if feature.created_by:
             email = feature.created_by.email()
         feature.creator = email
+        logging.info(f'updating {feature.name}.')
         feature.put(notify=False)
 
     logging.info(f'{update_count} features updated with new creator field.')
