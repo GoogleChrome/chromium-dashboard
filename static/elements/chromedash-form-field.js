d5038c021c55d893481ddee887ee37e728cf0e98
Daniel LaLiberte <dlaliberte@google.com>
2022-08-03 19:07:34 -0400

    Replace django checkbox with chromedash-form-field generated checkbox field. (#2066)
    * Replace django checkbox with chromedash-form-field generated checkbox field.
    
    * Generate form table row for checkbox in django without the form field
    
    * cleanup. remove unneeded test of 'Unlisted'
    
    * Use light DOM for field and errors.
    
    * Fix chromedash0form-field_test.
    
    * More cleanups.
    
    * Add test of "Unlisted" checkbox to chromedash-form-field_test.
    
    * Remove unused form-controller.
    
    * misc.
    
    * Workarounds for set_stage and feature_type.

--- static/elements/chromedash-form-field.js
+++ static/elements/chromedash-form-field.js
@@ -1,100 +1,17 @@
-import {LitElement, css, html} from 'lit';
+import {LitElement, html} from 'lit';
 import {ALL_FIELDS} from './form-field-specs';
+import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 
 export class ChromedashFormField extends LitElement {
   static get properties() {
     return {
       name: {type: String},
+      value: {type: String},
+      field: {type: String},
+      errors: {type: String},
+      stage: {type: String},
+      disabled: {type: Boolean},
     };
-  }
-
-  static get styles() {
-    // Remove vertical padding for extrahelp row cell.
-    // Rely on padding of sl-details, when it's displayed.
-
-    // If first child of helptext and sl-details content has
-    // margin-top, typically only block elements do,
-    // then make it 0.
-    // Similarly for last-child and margin-bottom.
-    return [
-      css`
-        :host {
-          display: table-row-group;
-        }
-
-        tr[hidden] th,
-        tr[hidden] td {
-          padding: 0;
-        }
-
-        th, td {
-          text-align: left;
-          vertical-align: top;
-        }
-
-        th {
-          padding: 12px 10px 5px 0;
-        }
-
-        td {
-          padding: 6px 10px;
-        }
-
-        td:first-of-type {
-          width: 60%;
-          max-width: 35em;
-        }
-
-        td.extrahelp {
-          padding: 0 10px;
-        }
-
-        .helptext {
-          display: block;
-          font-size: small;
-          max-width: 40em;
-          margin-top: 2px;
-        }
-
-        .helptext > *:first-child {
-          margin-top: 0;
-        }
-        .helptext > *:last-child {
-          margin-bottom: 0;
-        }
-
-        .errorlist {
-          color: red;
-        }
-
-        sl-details::part(base) {
-          border-width: 0;
-        }
-
-        sl-details::part(header) {
-          padding: 0;
-          display: none;
-        }
-
-        sl-details::part(content) {
-          padding-top: 0;
-        }
-
-        sl-icon-button::part(base) {
-          font-size: 16px;
-          color: var(--link-color);
-          padding: 0;
-          margin: 4px;
-        }
-
-        sl-details > *:first-child {
-          margin-top: 0;
-        }
-        sl-details > *:last-child {
-          margin-bottom: 0;
-        }
-      `,
-    ];
   }
 
   toggleExtraHelp() {
@@ -104,24 +21,58 @@ export class ChromedashFormField extends LitElement {
     button.name = details.open ? 'dash-square' : 'plus-square';
   }
 
+  // Must render to light DOM, so sl-form-fields work as intended.
+  createRenderRoot() {
+    return this;
+  }
+
   render() {
     const fieldProps = ALL_FIELDS[this.name] || {};
-    const label = fieldProps.label ? `${fieldProps.label}:` : '';
+    const label = fieldProps.label;
     const helpText = fieldProps.help_text || '';
     const extraHelpText = fieldProps.extra_help || '';
+    const type = fieldProps.type;
+
+    // If type is checkbox, then generate locally.
+    let fieldHTML = '';
+    if (type === 'checkbox') {
+      fieldHTML = html`
+          <sl-checkbox
+            name="${this.name}"
+            id="id_${this.name}"
+            size="small"
+            ?checked=${ this.value === 'True' ? true : false }
+            ?disabled=${ this.disabled }
+            >
+          ${label}
+        </sl-checkbox>
+        `;
+      this.generateFieldLocally = true;
+    } else {
+      // Temporary workaround until we migrate the generation of
+      // all the form fields to be done here.
+      // We pass the escaped html for the low-level form field
+      // through the field attribute, so it must be unescaped,
+      // and then we use unsafeHTML so it isn't re-escaped.
+      fieldHTML = unsafeHTML(unescape(this.field || ''));
+    }
+
+    // Similar to above, but maybe we can guarantee that the errors
+    // will never contain HTML.
+    const errorsHTML = unsafeHTML(unescape(this.errors || ''));
+
     return html`
       <tr>
         <th colspan="2">
           <b>
-            ${label}
-            <slot name="label"></slot>
+            ${label ? label + ':' : ''}
           </b>
         </th>
       </tr>
       <tr>
         <td>
-          <slot name="field"></slot>
-          <slot name="error" class="errorlist"></slot>
+          ${fieldHTML}
+          ${errorsHTML}
         </td>
         <td>
           <span class="helptext">
