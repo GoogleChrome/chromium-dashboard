f3892833c3e1ebe9b0725a2e131c8e54c51f4884
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-08-24 15:26:30 -0700

    string comparisons

--- static/elements/utils_test.js
+++ static/elements/utils_test.js
@@ -10,12 +10,13 @@ describe('utils', () => {
 go/this-is-a-test.
 A bug cr/1234 exists and also cl/1234. Info at issue 1234 comment 3.
 AKA issue 1234 #c3. https://example.com/ --- testing. bug 1234 also.`;
-      const expected = `This is a test of the autolinking.
+      const expected = html`${unsafeHTML(`This is a test of the autolinking.
 <a href="http://go/this-is-a-test" target="_blank" rel="noopener noreferrer">go/this-is-a-test</a>.
 A bug <a href="http://cr/1234" target="_blank" rel="noopener noreferrer">cr/1234</a> exists and also <a href="http://cl/1234" target="_blank" rel="noopener noreferrer">cl/1234</a>. Info at <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234#c3" target="_blank" rel="noopener noreferrer">issue 1234 comment 3</a>.
-AKA <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234#c3" target="_blank" rel="noopener noreferrer">issue 1234 #c3</a>. <a href="https://example.com/" target="_blank" rel="noopener noreferrer">https://example.com/</a> --- testing. <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234" target="_blank" rel="noopener noreferrer">bug 1234</a> also.`;
+AKA <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234#c3" target="_blank" rel="noopener noreferrer">issue 1234 #c3</a>. <a href="https://example.com/" target="_blank" rel="noopener noreferrer">https://example.com/</a> --- testing. <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234" target="_blank" rel="noopener noreferrer">bug 1234</a> also.`)}`;
+
       const result = autolink(before);
-      assert.equal(result, html`${unsafeHTML(expected)}`);
+      assert.equal(result.toString(), expected.toString());
     });
 
     it('does not change text with no links', () => {
@@ -23,12 +24,13 @@ AKA <a href="https://bugs.chromium.org/p/chromium/issues/detail?id=1234#c3" targ
 go this-is-a-test.
 A bug cr /1234 exists and also /1234. This is an example sentence.
 AKA issue here 1234. example com --- testing.`;
-      const expected = `This is a test of the autolinking.
+      const expected = html`${unsafeHTML(`This is a test of the autolinking.
 go this-is-a-test.
 A bug cr /1234 exists and also /1234. This is an example sentence.
-AKA issue here 1234. example com --- testing.`;
+AKA issue here 1234. example com --- testing.`)}`;
+
       const result = autolink(before);
-      assert.equal(result, html`${unsafeHTML(expected)}`);
+      assert.equal(result.toString(), expected.toString());
     });
   });
 });
