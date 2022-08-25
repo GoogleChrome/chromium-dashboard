1636632c752becee816fb9de2fa073712e066f5e
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-08-25 16:53:12 -0700

    Autolinking (#2167)
    * autolinking copy over from monorail
    
    * add tests and remove user email linking
    
    * test changes
    
    * more test updates
    
    * new line fix
    
    * string comparisons
    
    * move autolink to separate file
    
    * remove todo
    
    * fix import
    
    * Update package.json
    
    * Update package-lock.json
    
    * Revert "Update package-lock.json"
    
    This reverts commit 12fd140595e5887a99fea57a69a76ad1e6f3c6a3.
    
    * Revert "Update package.json"
    
    This reverts commit 8853ee9ffff099cd010bc6c3070d5586cddf75e4.
    
    * revert last change from package.json

--- static/elements/utils.js
+++ static/elements/utils.js
@@ -2,18 +2,15 @@
 
 import {html} from 'lit';
 import {unsafeHTML} from 'lit/directives/unsafe-html.js';
-import Autolinker from 'autolinker';
+import {markupAutolinks} from './autolink.js';
 
 let toastEl;
 
 /* Convert user-entered text into safe HTML with clickable links
  * where appropriate.  Returns a lit-html TemplateResult.
  */
-// TODO(jrobbins): autolink monorail-style issue references, go-links, etc.
 export function autolink(s) {
-  const markup = Autolinker.link(
-    s,
-    {stripPrefix: false, sanitizeHtml: true});
+  const markup = markupAutolinks(s);
   return html`${unsafeHTML(markup)}`;
 }
 
