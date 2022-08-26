9970ec3d7a0482f7baf0ea4d42f0976f3dde5fb7
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-08-26 15:30:55 -0700

    Enable new permissions system (#2174)
    * enable new permissions
    
    * review changes

--- api/permissions_api_test.py
+++ api/permissions_api_test.py
@@ -58,7 +58,7 @@ class PermissionsAPITest(testing_config.CustomTestCase):
     self.assertEqual(expected, actual)
 
   def test_get__googler(self):
-    """Googlers have default permissions to create and edit features."""
+    """Googlers have default permissions to create features."""
     testing_config.sign_in('one@google.com', 67890)
     with test_app.test_request_context(self.request_path):
       actual = self.handler.do_get()
@@ -66,7 +66,7 @@ class PermissionsAPITest(testing_config.CustomTestCase):
       'user': {
         'can_create_feature': True,
         'can_approve': False,
-        'can_edit_all': True,
+        'can_edit_all': False,
         'is_admin': False,
         'email': 'one@google.com',
         'editable_features': []
