a943ca4093599d7a05dc1c346d36dbce775c818e
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-08-31 14:59:40 -0700

    create standard_maturity backfill cron

---
+++ internals/deprecate_field.py
@@ -0,0 +1,23 @@
+import logging
+
+from internals.core_enums import STANDARD_MATURITY_BACKFILL
+from framework.basehandlers import FlaskHandler
+from internals.core_models import Feature
+
+class WriteStandardMaturityHandler(FlaskHandler):
+
+  def get_template_data(self):
+    """Writes standard_maturity field from standardization user field."""
+    q = Feature.query()
+    features = q.fetch()
+    update_count = 0
+    for feature in features:
+      if ((feature.standardization is not None and feature.standardization > 0)
+           and (feature.standard_maturity is None or feature.standard_maturity == 0)):
+        update_count += 1
+        feature.standard_maturity = STANDARD_MATURITY_BACKFILL[feature.standardization]
+        feature.put(notify=False)
+    
+    logging.info(
+        f'{update_count} features updated with standard_maturity field.')
+    return 'Success'
