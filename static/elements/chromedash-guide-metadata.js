eed9137ced018596dd6846eb67374354cf59899e
kevinshen56714 <kshen64@gatech.edu>
2022-09-03 13:01:29 -0400

    Resolve web test complaints

--- static/elements/chromedash-guide-metadata.js
+++ static/elements/chromedash-guide-metadata.js
@@ -1,5 +1,5 @@
 import {LitElement, css, html, nothing} from 'lit';
-import {ref} from 'lit/directives/ref';
+import {ref} from 'lit/directives/ref.js';
 import {autolink} from './utils.js';
 import './chromedash-form-table';
 import './chromedash-form-field';
