68e47ef1a9c4eda7452b83b4eae96cb5deebc1d7
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2022-09-28 10:23:23 -0700

    Comment IDs now migrate to Activity entities (#2274)
    * Comment ids not migrating
    
    * fix docstring
    
    * check for null or empty string content
    
    * Check for existing activities more efficiently

--- internals/schema_migration.py
+++ internals/schema_migration.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import logging
+from google.cloud import ndb
 
 from framework.basehandlers import FlaskHandler
 from internals.review_models import Activity, Comment
@@ -22,14 +23,21 @@ class MigrateCommentsToActivities(FlaskHandler):
   def get_template_data(self):
     """Writes an Activity entity for each unmigrated Comment entity."""
     self.require_cron_header()
-    q = Comment.query()
-    comments = q.fetch()
+
+    logging.info(self._remove_bad_id_activities())
+
+    comments = Comment.query().fetch()
+    activity_keys = Activity.query().fetch(keys_only=True)
+    activity_ids = set(key.integer_id() for key in activity_keys)
     migration_count = 0
     for comment in comments:
-      if comment.migrated:
+      # Check if an Activity with the same ID has already been created.
+      # If so, do not create this activity again.
+      if comment.key.integer_id() in activity_ids:
         continue
 
       kwargs = {
+        'id': comment.key.integer_id(),
         'feature_id': comment.feature_id,
         'gate_id': comment.field_id,
         'author': comment.author,
@@ -39,10 +47,30 @@ class MigrateCommentsToActivities(FlaskHandler):
       }
       activity = Activity(**kwargs)
       activity.put()
-      comment.migrated = True
-      comment.put()
       migration_count += 1
 
     message = f'{migration_count} comments migrated to activity entities.'
     logging.info(message)
     return message
+  
+  def _remove_bad_id_activities(self):
+    """Deletes old Activity entities that do not have a matching comment ID."""
+    q = Activity.query()
+    activities = q.fetch()
+
+    old_migrations_deleted = 0
+    for activity in activities:
+      # Non-empty content field means this is an Activity entity
+      # that represents a comment.
+      if activity.content:
+        # Check if there is a Comment entity with a matching ID.
+        q = Comment.query().filter(
+            Comment.key == ndb.Key(Comment, activity.key.integer_id()))
+        comments_with_same_id = q.fetch()
+        if len(comments_with_same_id) != 1:
+          # If not, it is from the old migration and it can be deleted.
+          activity.key.delete()
+          old_migrations_deleted += 1
+    
+    return (f'{old_migrations_deleted} Activities deleted '
+        'from previous migration.')
