6f78d97f6ff9cde0c3c7f57e11314baacb9e0ad2
pingren <5123601+Pingren@users.noreply.github.com>
2023-05-28 21:22:52 +0800

    Change tab size to 2 space

--- internals/link_helpers.py
+++ internals/link_helpers.py
@@ -16,71 +16,71 @@ LINK_TYPES_REGEX = {
 
 class Link():
 
-    @classmethod
-    def get_type(cls, link: str) -> str | None:
-        """Return Link_Type if the given link is valid. Otherwise, return None."""
-        for link_type, regex in LINK_TYPES_REGEX.items():
-            if regex.match(link):
-                return link_type
-        return None
+  @classmethod
+  def get_type(cls, link: str) -> str | None:
+    """Return Link_Type if the given link is valid. Otherwise, return None."""
+    for link_type, regex in LINK_TYPES_REGEX.items():
+      if regex.match(link):
+        return link_type
+    return None
 
-    def __init__(self, url: str):
-        self.url = url
-        self.type = Link.get_type(url)
-        self.is_parsed = False
-        self.is_error = False
+  def __init__(self, url: str):
+    self.url = url
+    self.type = Link.get_type(url)
+    self.is_parsed = False
+    self.is_error = False
+    self.information = None
+
+  def _parse_chromium_bug(self) -> dict[str, object]:
+    """Parse the information from the chromium bug tracker."""
+
+    endpoint = 'https://bugs.chromium.org/prpc/monorail.Issues/GetIssue'
+
+    issue_id = self.url.split('id=')[-1]
+
+    # csrf token is required, its expiration is about 2 hours according to the tokenExpiresSec field
+    # technically, we could cache the csrf token and reuse it for 2 hours
+
+    csrf_token = re.findall(
+        "'token': '(.*?)'", requests.get("https://bugs.chromium.org/p/chromium/issues/wizard").text)
+    csrf_token = csrf_token[0] if csrf_token else None
+
+    if csrf_token is None:
+      raise Exception("Could not find bugs.chromium.org CSRF token")
+
+    headers = {
+        'accept': 'application/json',
+        'x-xsrf-token': str(csrf_token),
+        'content-type': 'application/json',
+    }
+    body = {
+        'issueRef': {
+            'projectName': 'chromium',
+            'localId': int(issue_id)
+        },
+    }
+
+    json_str = requests.post(endpoint, json=body, headers=headers).text
+
+    # remove )]}' from the beginning of the response
+    if json_str.startswith(")]}'"):
+      json_str = json_str[5:]
+
+    information: dict[str, Any] = json.loads(json_str)
+
+    return information.get('issue', None)
+
+  def parse(self):
+    """Parse the link and store the information."""
+
+    if self.type == LINK_TYPE_CHROMIUM_BUG:
+      try:
+        self.information = self._parse_chromium_bug()
+      except Exception as e:
+        logging.error(f'Error parsing chromium bug {self.url}: {e}')
+        self.is_error = True
         self.information = None
-
-    def _parse_chromium_bug(self) -> dict[str, object]:
-        """Parse the information from the chromium bug tracker."""
-
-        endpoint = 'https://bugs.chromium.org/prpc/monorail.Issues/GetIssue'
-
-        issue_id = self.url.split('id=')[-1]
-
-        # csrf token is required, its expiration is about 2 hours according to the tokenExpiresSec field
-        # technically, we could cache the csrf token and reuse it for 2 hours
-
-        csrf_token = re.findall(
-            "'token': '(.*?)'", requests.get("https://bugs.chromium.org/p/chromium/issues/wizard").text)
-        csrf_token = csrf_token[0] if csrf_token else None
-
-        if csrf_token is None:
-            raise Exception("Could not find bugs.chromium.org CSRF token")
-
-        headers = {
-            'accept': 'application/json',
-            'x-xsrf-token': str(csrf_token),
-            'content-type': 'application/json',
-        }
-        body = {
-            'issueRef': {
-                'projectName': 'chromium',
-                'localId': int(issue_id)
-            },
-        }
-
-        json_str = requests.post(endpoint, json=body, headers=headers).text
-
-        # remove )]}' from the beginning of the response
-        if json_str.startswith(")]}'"):
-            json_str = json_str[5:]
-
-        information: dict[str, Any] = json.loads(json_str)
-
-        return information.get('issue', None)
-
-    def parse(self):
-        """Parse the link and store the information."""
-
-        if self.type == LINK_TYPE_CHROMIUM_BUG:
-            try:
-                self.information = self._parse_chromium_bug()
-            except Exception as e:
-                logging.error(f'Error parsing chromium bug {self.url}: {e}')
-                self.is_error = True
-                self.information = None
-        elif self.type == LINK_TYPE_UNKNOWN:
-            # TODO: parse other url title and description, og tags, etc.
-            self.information = None
-        self.is_parsed = True
+    elif self.type == LINK_TYPE_UNKNOWN:
+      # TODO: parse other url title and description, og tags, etc.
+      self.information = None
+    self.is_parsed = True
--- internals/link_helpers_test.py
+++ internals/link_helpers_test.py
@@ -3,35 +3,32 @@ from internals.link_helpers import Link, LINK_TYPE_CHROMIUM_BUG
 
 
 class LinkHelperTest(testing_config.CustomTestCase):
+  def test_link_with_wrong_host(self):
+    link = Link("https://bugs0chromium.org/p/chromium/issues/detail?id=100000")
+    assert link.type != LINK_TYPE_CHROMIUM_BUG
 
-    def test_link_with_wrong_host(self):
-        link = Link(
-                'https://bugs0chromium.org/p/chromium/issues/detail?id=100000'
-                )
-        assert link.type != LINK_TYPE_CHROMIUM_BUG
-    
-    def test_parse_chromium_tracker(self):
-        link = Link(
-            'https://bugs.chromium.org/p/chromium/issues/detail?id=100000')
-        link.parse()
-        info = link.information
-        assert link.type == LINK_TYPE_CHROMIUM_BUG
-        assert link.is_parsed == True
-        assert info["summary"] == "Repeated zooms leave tearing artifact"
-        assert info["statusRef"]["status"] == "Fixed"
-        assert info["ownerRef"]["displayName"] == 'backer@chromium.org'
+  def test_parse_chromium_tracker(self):
+    link = Link("https://bugs.chromium.org/p/chromium/issues/detail?id=100000")
+    link.parse()
+    info = link.information
+    assert link.type == LINK_TYPE_CHROMIUM_BUG
+    assert link.is_parsed == True
+    assert info["summary"] == "Repeated zooms leave tearing artifact"
+    assert info["statusRef"]["status"] == "Fixed"
+    assert info["ownerRef"]["displayName"] == "backer@chromium.org"
 
-    def test_parse_chromium_tracker_fail_wrong_id(self):
-        link = Link(
-            'https://bugs.chromium.org/p/chromium/issues/detail?id=100000000000000')
-        link.parse()
-        assert link.information == None 
-        assert link.is_parsed == True
-        assert link.is_error == True
+  def test_parse_chromium_tracker_fail_wrong_id(self):
+    link = Link(
+        "https://bugs.chromium.org/p/chromium/issues/detail?id=100000000000000"
+    )
+    link.parse()
+    assert link.information == None
+    assert link.is_parsed == True
+    assert link.is_error == True
 
-    def test_parse_chromium_tracker_fail_no_permission(self):
-        link = Link('https://bugs.chromium.org/p/chromium/issues/detail?id=1')
-        link.parse()
-        assert link.information == None
-        assert link.is_parsed == True 
-        assert link.is_error == True
+  def test_parse_chromium_tracker_fail_no_permission(self):
+    link = Link("https://bugs.chromium.org/p/chromium/issues/detail?id=1")
+    link.parse()
+    assert link.information == None
+    assert link.is_parsed == True
+    assert link.is_error == True
