66275d13671eb9ab00cc906e3c37320a1da0b9a3
pingren <5123601+Pingren@users.noreply.github.com>
2023-05-29 00:08:36 +0800

    Use unittest assert

--- internals/link_helpers_test.py
+++ internals/link_helpers_test.py
@@ -5,30 +5,32 @@ from internals.link_helpers import Link, LINK_TYPE_CHROMIUM_BUG
 class LinkHelperTest(testing_config.CustomTestCase):
   def test_link_with_wrong_host(self):
     link = Link("https://bugs0chromium.org/p/chromium/issues/detail?id=100000")
-    assert link.type != LINK_TYPE_CHROMIUM_BUG
+    self.assertNotEqual(link.type, LINK_TYPE_CHROMIUM_BUG)
 
   def test_parse_chromium_tracker(self):
     link = Link("https://bugs.chromium.org/p/chromium/issues/detail?id=100000")
     link.parse()
     info = link.information
-    assert link.type == LINK_TYPE_CHROMIUM_BUG
-    assert link.is_parsed == True
-    assert info["summary"] == "Repeated zooms leave tearing artifact"
-    assert info["statusRef"]["status"] == "Fixed"
-    assert info["ownerRef"]["displayName"] == "backer@chromium.org"
+    self.assertEqual(link.type, LINK_TYPE_CHROMIUM_BUG)
+    self.assertEqual(link.is_parsed, True)
+    self.assertEqual(info["summary"], "Repeated zooms leave tearing artifact")
+    self.assertEqual(info["statusRef"]["status"], "Fixed")
+    self.assertEqual(info["ownerRef"]["displayName"], "backer@chromium.org")
 
   def test_parse_chromium_tracker_fail_wrong_id(self):
     link = Link(
         "https://bugs.chromium.org/p/chromium/issues/detail?id=100000000000000"
     )
     link.parse()
-    assert link.information == None
-    assert link.is_parsed == True
-    assert link.is_error == True
+    self.assertEqual(link.type, LINK_TYPE_CHROMIUM_BUG)
+    self.assertEqual(link.is_parsed, True)
+    self.assertEqual(link.is_error, True)
+    self.assertEqual(link.information, None)
 
   def test_parse_chromium_tracker_fail_no_permission(self):
     link = Link("https://bugs.chromium.org/p/chromium/issues/detail?id=1")
     link.parse()
-    assert link.information == None
-    assert link.is_parsed == True
-    assert link.is_error == True
+    self.assertEqual(link.type, LINK_TYPE_CHROMIUM_BUG)
+    self.assertEqual(link.is_parsed, True)
+    self.assertEqual(link.is_error, True)
+    self.assertEqual(link.information, None)
