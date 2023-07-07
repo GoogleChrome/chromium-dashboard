7ded2325cc0a88840049268d73abe5e7fa41ceac
Jason Robbins <jrobbins@gmail.com>
2023-07-07 02:39:54 +0000

    Select client tokens based on least recent failure.

--- framework/secrets.py
+++ framework/secrets.py
@@ -91,10 +91,11 @@ class ApiCredential(ndb.Model):
   """A server-side-only list of values that we use to access APIs."""
   api_name = ndb.StringProperty(required=True)
   token = ndb.StringProperty()
+  failure_timestamp = ndb.IntegerProperty(default=0)
 
   @classmethod
-  def randomly_select_token_for_api(cls, api_name: str) -> str | None:
-    """Return one of our tokens for the requested API or None."""
+  def select_token_for_api(cls, api_name: str) -> 'ApiCredential':
+    """Return one of our credientialsfor the requested API or make a blank."""
     query = ApiCredential.query(ApiCredential.api_name == api_name)
     all_for_api = query.fetch(None)
     if not all_for_api:
@@ -102,15 +103,21 @@ class ApiCredential(ndb.Model):
       blank_entry.put()
       logging.info('Created an ApiCredential for %r', api_name)
       logging.info('Please use the Cloud Console to fill in a token')
-      return None
+      return blank_entry
 
-    # If ndb has multiple tokens for the same API, choose randomly
-    # among them so that different server instances will use different
-    # tokens and spend the quota of each token roughly evenly.
+    # If ndb has multiple tokens for the same API, choose one
+    # that has not failed recently for any reason (including quota).
     logging.info('Found %r tokens for %r', len(all_for_api), api_name)
-    return random.choice(all_for_api).token
+    sorted_for_api = sorted(all_for_api, key=lambda ac: ac.failure_timestamp)
+    return sorted_for_api[0]
 
   @classmethod
-  def get_github_token(cls) -> str | None:
-    """Return one of our GitHub tokens or None."""
-    return cls.randomly_select_token_for_api(GITHUB_API_NAME)
+  def get_github_credendial(cls) -> 'ApiCredential':
+    """Return an ApiCredential for GitHub."""
+    return cls.select_token_for_api(GITHUB_API_NAME)
+
+  def record_failure(self, now=None) -> None:
+    """Mark this ApiCredential as failing now."""
+    logging.info('Recording failure at %r', now or int(time.time()))
+    self.failure_timestamp = now or int(time.time())
+    self.put()
