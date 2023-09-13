645607691389cf754d6f7d04cd13a755ff434fd7
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2023-09-13 12:46:44 -0700

    `stage_description` -> `ot_description`
    With a more detailed description of the field and its relation to OT

--- client-src/elements/form-field-enums.js
+++ client-src/elements/form-field-enums.js
@@ -241,7 +241,6 @@ export const STAGE_SPECIFIC_FIELDS = new Set([
 
   // Misc fields.
   'display_name',
-  'stage_description',
   'origin_trial_feedback_url',
   'ot_chromium_trial_name',
   'ot_description',
