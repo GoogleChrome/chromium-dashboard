06c2deaece3fbae0eaa23be90dabc36c10d2fae6
Daniel LaLiberte <dlaliberte@google.com>
2024-01-10 17:01:13 +0000

    Move semantic checks of feature name and summary length.

--- packages/playwright/tests/chromedash-guide-editall-page_pwtest.js
+++ packages/playwright/tests/chromedash-guide-editall-page_pwtest.js
@@ -42,25 +42,3 @@ async function gotoNewFeaturePage(page) {
     await expect(addAFeatureHeader).toBeVisible({ timeout: 10000 });
     // console.log('navigate to create feature page done');
 }
-
-test('test semantic checks', async ({ page }) => {
-    await gotoNewFeaturePage(page);
-
-    // Enter feature name
-    const featureNameInput = page.locator('input[name="name"]');
-    await featureNameInput.fill('Test deprecated feature name');
-    await delay(500);
-
-    // Enter summary description
-    const summaryInput = page.locator('textarea[name="summary"]');
-    await summaryInput.fill('Test summary description');
-    await summaryInput.blur(); // Must blur to trigger change event.
-
-    await delay(500);
-
-    // Screenshot of this warning about summary length
-    await expect(page).toHaveScreenshot('warning-feature-name-and-summary-length.png', {
-        mask: [page.locator('section[id="history"]')]
-    });
-    await delay(500);
-});
