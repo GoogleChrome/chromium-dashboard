27eb18c999a723665a4bfe137f44cda7cf5dab0f
Jeffrey Yasskin <jyasskin@google.com>
2024-01-25 00:48:22 +0000

    Update openapi-cli to 7.2.0.

--- gen/py/chromestatus_openapi/chromestatus_openapi/models/__init__.py
+++ gen/py/chromestatus_openapi/chromestatus_openapi/models/__init__.py
@@ -1,7 +1,4 @@
-# coding: utf-8
-
 # flake8: noqa
-from __future__ import absolute_import
 # import models into model package
 from chromestatus_openapi.models.component_users_request import ComponentUsersRequest
 from chromestatus_openapi.models.components_user import ComponentsUser
