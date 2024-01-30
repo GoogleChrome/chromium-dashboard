2e7182df772bf3d196883ede11c1347d2bbfcdea
Jeffrey Yasskin <jyasskin@google.com>
2024-01-30 00:41:10 +0000

    Format the seed_datastore.py script with Ruff.

--- scripts/seed_datastore.py
+++ scripts/seed_datastore.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Fetches features from the staging or live dashboard site, and inserts them into the development
 datastore.
@@ -21,12 +20,12 @@ datastore.
 This script can also set a user as admin, to facilitate testing those parts of the site.
 """
 
-from datetime import datetime
-from urllib.parse import urljoin
 import argparse
 import logging
 import os
 import sys
+from datetime import datetime
+from urllib.parse import urljoin
 
 import requests
 from google.cloud import ndb  # type: ignore
@@ -39,48 +38,49 @@ os.environ.setdefault('GOOGLE_CLOUD_PROJECT', 'cr-status-staging')
 os.environ.setdefault('SERVER_SOFTWARE', 'gunicorn')
 
 # pylint: disable=wrong-import-position
+# ruff: noqa: E402
 from internals.core_enums import MISC
 from internals.core_models import FeatureEntry
 from internals.user_models import AppUser
 
 
 def add_features(server: str, after: datetime):
-    """Fetches features from `server` and inserts them into the dev datastore."""
-    target = urljoin(server, 'features.json')
-    logging.info('Fetching features from %s.', target)
-    features = requests.get(target, timeout=120)
-    features.raise_for_status()
+  """Fetches features from `server` and inserts them into the dev datastore."""
+  target = urljoin(server, 'features.json')
+  logging.info('Fetching features from %s.', target)
+  features = requests.get(target, timeout=120)
+  features.raise_for_status()
 
-    parsed_features = features.json()
-    recent_features = [
-        f for f in parsed_features
-        if after < datetime.fromisoformat(f['updated']['when'])
-    ]
-    logging.info('Adding %d recent features of %d total to the datastore.',
-                 len(recent_features), len(parsed_features))
+  parsed_features = features.json()
+  recent_features = [
+      f for f in parsed_features
+      if after < datetime.fromisoformat(f['updated']['when'])
+  ]
+  logging.info('Adding %d recent features of %d total to the datastore.',
+               len(recent_features), len(parsed_features))
 
-    for f in recent_features:
-        fe = FeatureEntry(id=f['id'],
-                          created=datetime.fromisoformat(f['created']['when']),
-                          updated=datetime.fromisoformat(f['updated']['when']))
-        fe.name = f['name']
-        fe.summary = f['summary']
-        fe.category = f.get('category_int', MISC)
-        # fe.feature_type = f['feature_type']  # Not available from the dump?
-        fe.impl_status_chrome = f['browsers']['chrome']['status']['val']
-        fe.standard_maturity = f['standards']['maturity']['val']
-        fe.ff_views = f['browsers']['ff']['view']['val']
-        fe.safari_views = f['browsers']['safari']['view']['val']
-        fe.web_dev_views = f['browsers']['webdev']['view']['val']
-        fe.put()
+  for f in recent_features:
+    fe = FeatureEntry(id=f['id'],
+                      created=datetime.fromisoformat(f['created']['when']),
+                      updated=datetime.fromisoformat(f['updated']['when']))
+    fe.name = f['name']
+    fe.summary = f['summary']
+    fe.category = f.get('category_int', MISC)
+    # fe.feature_type = f['feature_type']  # Not available from the dump?
+    fe.impl_status_chrome = f['browsers']['chrome']['status']['val']
+    fe.standard_maturity = f['standards']['maturity']['val']
+    fe.ff_views = f['browsers']['ff']['view']['val']
+    fe.safari_views = f['browsers']['safari']['view']['val']
+    fe.web_dev_views = f['browsers']['webdev']['view']['val']
+    fe.put()
 
 
 def add_admin(email: str):
-    """Makes the user named `email` an admin."""
-    user = AppUser()
-    user.email = email
-    user.is_admin = True
-    user.put()
+  """Makes the user named `email` an admin."""
+  user = AppUser()
+  user.email = email
+  user.is_admin = True
+  user.put()
 
 
 parser = argparse.ArgumentParser(description='seed the development datastore')
@@ -93,15 +93,16 @@ parser.add_argument('--admin',
 parser.add_argument(
     '--after',
     type=datetime.fromisoformat,
-    help='only add features that have been updated since AFTER (in ISO date format)',
+    help=
+    'only add features that have been updated since AFTER (in ISO date format)',
     default='0001-01-01')
 
 args = parser.parse_args()
 client = ndb.Client()
 with client.context():
-    if args.server:
-        add_features(server=args.server, after=args.after)
+  if args.server:
+    add_features(server=args.server, after=args.after)
 
-    if args.admin:
-        logging.info('Making %s an admin.', args.admin)
-        add_admin(args.admin)
+  if args.admin:
+    logging.info('Making %s an admin.', args.admin)
+    add_admin(args.admin)
