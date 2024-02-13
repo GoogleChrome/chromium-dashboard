474a174ce9ee39e37b00962bb6854f924017c84f
Jeffrey Yasskin <jyasskin@google.com>
2024-02-13 22:19:37 +0000

    Also test the 'after' query param and form field.

--- packages/playwright/tests/chromedash-report-spec-mentors-page_pwtest.js
+++ packages/playwright/tests/chromedash-report-spec-mentors-page_pwtest.js
@@ -32,3 +32,31 @@ test('spec mentors report renders', async ({page}) => {
   await expect.soft(mentorLinks.nth(1)).toHaveText("First feature");
   await expect.soft(mentorLinks.nth(1)).toHaveAttribute("href", /\/feature\/9009/);
 });
+
+test('after query param affects api request', async ({page}) => {
+  await page.route('/api/v0/spec_mentors?after=*', route => route.fulfill({
+    status: 200,
+    body: `)]}'\n${JSON.stringify(spec_mentor_api_result)}`,
+  }));
+
+  const apiRequest = page.waitForRequest(/\/api\/v0\/spec_mentors\?after=2023-04-05$/);
+  await page.goto('/reports/spec_mentors?after=2023-04-05');
+  await apiRequest;
+
+  // Should be reflected in the form field.
+  await expect(page.getByLabel('updated after')).toHaveValue('2023-04-05');
+});
+
+test('after form field affects api request and page url', async ({page}) => {
+  await page.route('/api/v0/spec_mentors?after=*', route => route.fulfill({
+    status: 200,
+    body: `)]}'\n${JSON.stringify(spec_mentor_api_result)}`,
+  }));
+
+  await page.goto('/reports/spec_mentors');
+
+  const apiRequest = page.waitForRequest(/\/api\/v0\/spec_mentors\?after=2023-05-06$/);
+  await page.getByLabel('updated after').fill('2023-05-06');
+  await apiRequest;
+  await expect(page).toHaveURL('/reports/spec_mentors?after=2023-05-06')
+});
