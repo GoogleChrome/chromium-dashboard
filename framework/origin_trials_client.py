0f79caf190ee314bfc49c3b23bfe0efdf123e8a6
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2024-02-20 11:53:53 -0800

    add trial extension function to OT client

--- framework/origin_trials_client.py
+++ framework/origin_trials_client.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import google.auth
+from google.auth.transport.requests import Request
 
 from dataclasses import asdict
 import logging
@@ -21,9 +23,6 @@ import requests
 from framework import secrets
 from internals.data_types import OriginTrialInfo
 import settings
-
-
-
 
 
 def get_trials_list() -> list[dict[str, Any]]:
@@ -42,13 +41,34 @@ def get_trials_list() -> list[dict[str, Any]]:
   if key == None:
     return []
 
+  credentials, project_id = google.auth.default(scopes=[
+      "https://www.googleapis.com/auth/chromeorigintrials"])
+  credentials.refresh(Request())
+  # credentials = app_engine.Credentials(scopes=[
+  #     "https://www.googleapis.com/auth/chromeorigintrials"], service_account_id='113400961428040496994', quota_project_id='cr-status-staging')
+  token_header = f'Bearer {credentials.token}'
+  response = requests.post(
+      f'{settings.OT_API_URL}/v1/trials/4544132024016830465:add_extension?key={key}',
+      headers={'Authorization': token_header},
+      json={
+        "trial_id": "4544132024016830465",
+        "end_date": {
+            "seconds": 2008384422
+        },
+        "milestone_end": "250",
+        "extension_intent_url": "https://example.com/from_chromestatus2"
+      })
+  return str({
+    'token_header': token_header,
+    'response': response.json()})
+  # return str(credentials.__dict__)
   try:
     response = requests.get(
         f'{settings.OT_API_URL}/v1/trials',
         params={'prettyPrint': 'false', 'key': key})
     response.raise_for_status()
   except requests.exceptions.RequestException as e:
-    logging.exception("Failed to get response from origin trials API.")
+    logging.exception('Failed to get response from origin trials API.')
     raise e
 
   response_json = response.json()
@@ -57,3 +77,44 @@ def get_trials_list() -> list[dict[str, Any]]:
                  for api_trial in response_json['trials']
                  if api_trial['isPublic']]
   return trials_list
+
+
+def extend_origin_trial(trial_id: str, milestone_end: str, intent_url: str):
+  """Extend an existing origin trial.
+
+  Returns:
+    Boolean value if the extension request was successful.
+
+  Raises:
+    requests.exceptions.RequestException: If the request fails to connect or
+      the HTTP status code is not successful.
+  """
+  key = secrets.get_ot_api_key()
+  # Return False if no API key is found.
+  if key == None:
+    return False
+
+  end_seconds = 0
+  # Obtain the service account credentials to be used in the request
+  # using the origin trials auth scope.
+  credentials, _ = google.auth.default(scopes=[
+      'https://www.googleapis.com/auth/chromeorigintrials'])
+  credentials.refresh(Request())
+  url = (f'{settings.OT_API_URL}/v1/trials/{trial_id}:add_extension'
+         f'key={key}')
+  headers = {'Authorization': f'Bearer {credentials.token}'}
+  json = {
+    'trial_id': trial_id,
+    'end_date': {
+      'seconds': end_seconds
+    },
+    'extension_intent_url': intent_url,
+  }
+
+  try:
+    response = requests.post(url, headers=headers, json=json)
+  except requests.exceptions.RequestException as e:
+    logging.exception('Failed to get response from origin trials API.')
+    raise e
+
+  return response.status_code == 200
