4aec75440b09b54f04d82e289e6e5a3c3fa542ad
Jason Robbins <jrobbins@gmail.com>
2024-03-21 19:42:33 +0000

    Refactored for testibility

--- api/feature_latency_api.py
+++ api/feature_latency_api.py
@@ -59,7 +59,19 @@ class FeatureLatencyAPI(basehandlers.APIHandler):
     start_date, end_date = self.get_date_range(self.request.args)
     logging.info('range %r %r', start_date, end_date)
 
-    # 1. Get all shipped feature entries that were created before end date.
+    features = self.get_features_to_consider(start_date, end_date)
+    ship_milestone_by_fid = self.get_shipped_milestones(features)
+    milestone_details = self.get_milestone_details(ship_milestone_by_fid)
+    matching_features = self.filter_out_unshipped_features(
+        features, ship_milestone_by_fid, start_date, end_date,
+        milestone_details)
+    result = self.convert_to_result_format(
+        matching_features, ship_milestone_by_fid, milestone_details)
+    return result
+
+  def get_features_to_consider(
+      self, start_date: datetime, end_date: int) -> list[FeatureEntry]:
+    """Get all shipped feature entries that were created before end date."""
     fe_query = FeatureEntry.query().order(FeatureEntry.created)
     fe_query = fe_query.filter(
         FeatureEntry.created > start_date - timedelta(days=365))
@@ -70,12 +82,14 @@ class FeatureLatencyAPI(basehandlers.APIHandler):
         fe for fe in features
         if (not fe.deleted and
             fe.impl_status_chrome in [ENABLED_BY_DEFAULT, DEPRECATED, REMOVED])]
+    return features
 
-    # 2. Get all the ship Stages for those features to find shipping milestones.
+  def get_shipped_milestones(self, features):
+    """Get all the ship Stages for those features to find shipping milestones."""
     feature_ids = {fe.key.integer_id() for fe in features}
     logging.info('feature_ids %r', feature_ids)
     if not feature_ids:
-      return []
+      return {}
     stage_query = Stage.query()
     stage_query = stage_query.filter(Stage.feature_id.IN(feature_ids))
     stage_query = stage_query.filter(Stage.stage_type.IN([
@@ -84,31 +98,45 @@ class FeatureLatencyAPI(basehandlers.APIHandler):
     stages = stage_query.fetch(None)
     stages = [s for s in stages if not s.archived]
     if not stages:
-      return []
+      return {}
     stages_by_fid = stage_helpers.organize_all_stages_by_feature(stages)
+
     ship_milestone_by_fid = {}
     for f_id, feature_stages in stages_by_fid.items():
       for s in feature_stages:
-        if s and s.milestones:
-          if s.milestones.desktop_first:
-            if (ship_milestone_by_fid.get(f_id, 9999) >
-                s.milestones.desktop_first):
-              ship_milestone_by_fid[f_id] = s.milestones.desktop_first
-          if s.milestones.android_first:
-            if (ship_milestone_by_fid.get(f_id, 9999) >
-                s.milestones.android_first):
-              ship_milestone_by_fid[f_id] = s.milestones.android_first
+        if not s or not s.milestones:
+          continue
+        if s.milestones.desktop_first:
+          if (ship_milestone_by_fid.get(f_id, 9999) >
+              s.milestones.desktop_first):
+            ship_milestone_by_fid[f_id] = s.milestones.desktop_first
+        if s.milestones.android_first:
+          if (ship_milestone_by_fid.get(f_id, 9999) >
+              s.milestones.android_first):
+            ship_milestone_by_fid[f_id] = s.milestones.android_first
 
-    # 3. Get all the milestone details, including branch date.
+    return ship_milestone_by_fid
+
+  def get_milestone_details(self, ship_milestone_by_fid):
+    """Get all the milestone details, including branch date."""
+    if not ship_milestone_by_fid:
+      return []
     milestone_details = channels_api.construct_specified_milestones_details(
         min(ship_milestone_by_fid.values()),
         max(ship_milestone_by_fid.values()))
     for m in milestone_details:
-      logging.info('M%d: branch_point %r', m,
-                   milestone_details[m].get('branch_point'))
+      logging.info(
+          'M%d: branch_point %r', m, milestone_details[m].get('branch_point'))
 
-    # 4. Use only features that shipped in milestones that branched
-    # between start_date and end_date.
+    return milestone_details
+
+  def filter_out_unshipped_features(
+      self, features, ship_milestone_by_fid, start_date, end_date,
+      milestone_details):
+    """Return only features that shipped in milestones that branched
+       between start_date and end_date."""
+    if not milestone_details:
+      return []
     start_date_iso = start_date.isoformat()
     end_date_iso = end_date.isoformat()
     lowest_milestone_in_range = min(
@@ -118,16 +146,19 @@ class FeatureLatencyAPI(basehandlers.APIHandler):
       m for m in milestone_details
       if milestone_details[m].get('branch_point', '9') <= end_date_iso)
     logging.info('highest_milestone_in_range: %r', highest_milestone_in_range)
-    features = [
+    matching_features = [
         fe for fe in features
         if (ship_milestone_by_fid.get(fe.key.integer_id()) and
             lowest_milestone_in_range <=
             ship_milestone_by_fid[fe.key.integer_id()] <=
             highest_milestone_in_range)]
+    return matching_features
 
-    # 5. Stuff results into OpenAPI objects and convert to python dicts.
+  def convert_to_result_format(
+      self, matching_features, ship_milestone_by_fid, milestone_details):
+    """Stuff results into OpenAPI objects and convert to python dicts."""
     result = []
-    for fe in features:
+    for fe in matching_features:
       m = ship_milestone_by_fid.get(fe.key.integer_id())
       if m and milestone_details[m].get('branch_point'):
         result.append(FeatureLatency(
