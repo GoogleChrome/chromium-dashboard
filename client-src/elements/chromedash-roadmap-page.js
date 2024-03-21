07f27ef1a12df307d42dce36a8b8376539114dd2
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-03-21 09:17:14 +0530

    updated fix for Toast is briefly displayed on page load

--- client-src/elements/chromedash-roadmap-page.js
+++ client-src/elements/chromedash-roadmap-page.js
@@ -57,10 +57,6 @@ export class ChromedashRoadmapPage extends LitElement {
 
   firstUpdated() {
     this.handleResize();
-
-    const toastEl = document.createElement('chromedash-toast');
-    document.body.appendChild(toastEl);
-    toastEl.showMessage("Welcome to chromestatus.com!");
   }
 
   handleResize() {
