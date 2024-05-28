5b4c4c1a8d062cc703c6950905c4f44a8241f8ca
Jeffrey Yasskin <jyasskin@google.com>
2024-05-28 23:41:55 +0000

    Dismiss the Google Sign-in dialog, if it's present.

--- packages/playwright/tests/chromedash-old-features-page_pwtest.js
+++ packages/playwright/tests/chromedash-old-features-page_pwtest.js
@@ -1,5 +1,5 @@
 // @ts-check
-import {expect, test} from '@playwright/test';
+import {errors, expect, test} from '@playwright/test';
 
 test('new features page link is clickable while features are loading', async ({
   page,
@@ -20,7 +20,23 @@ test('new features page link is clickable while features are loading', async ({
     name: 'Try out our new features page',
   });
   await expect(newFeaturesLocator).toBeVisible();
-  await newFeaturesLocator.click({timeout: 5000});
+  try {
+    await newFeaturesLocator.click({timeout: 500});
+  } catch (e) {
+    const signInDialogLocator = page.getByTitle('Sign in with Google Dialog');
+    // If this is a timeout because the Google sign-in dialog is present.
+    if (
+      e instanceof errors.TimeoutError &&
+      (await signInDialogLocator?.isVisible())
+    ) {
+      // Dismiss the dialog.
+      await signInDialogLocator.press('Escape');
+      // And retry the click.
+      await newFeaturesLocator.click({timeout: 5000});
+    } else {
+      throw e;
+    }
+  }
   await expect(page).toHaveURL('/newfeatures');
 
   // Don't leave the network request hanging forever.
