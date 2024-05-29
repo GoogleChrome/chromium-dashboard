b6d62c6a30b253f94e66acc446a8fa4796693892
Jeffrey Yasskin <jyasskin@google.com>
2024-05-29 08:32:44 -0700

    Save a screenshot when a test fails. (#3921)

--- packages/playwright/playwright.config.cjs
+++ packages/playwright/playwright.config.cjs
@@ -34,6 +34,9 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    // See https://playwright.dev/docs/test-use-options#recording-options.
+    screenshot: 'only-on-failure',
   },
   preserveOutput: 'always',
 
