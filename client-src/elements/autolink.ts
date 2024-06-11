3da5e99c427d2fd1b81fe96d40e97e219ba96638
markxiong0122 <markxiong0122@gmail.com>
2024-06-10 22:45:43 -0500

    auto-link

--- client-src/elements/autolink.ts
+++ client-src/elements/autolink.ts
@@ -20,7 +20,7 @@ const NUMERIC_SHORT_LINK_RE =
 const IMPLIED_LINK_RE =
   /(?!@)(^|[^-\/._])\b[a-z]((-|\.)?[a-z0-9])+\.(com|net|org|edu|dev)\b(\/[^\s<]*)?/gi;
 const IS_LINK_RE = /()\b(https?:\/\/|ftp:\/\/|mailto:)([^\s<]+)/gi;
-const LINK_TRAILING_CHARS = [
+const LINK_TRAILING_CHARS: [string | null, string] [] = [
   [null, ':'],
   [null, '.'],
   [null, ','],
@@ -34,7 +34,7 @@ const LINK_TRAILING_CHARS = [
 const GOOG_SHORT_LINK_RE =
   /^(b|t|o|omg|cl|cr|go|g|shortn|who|teams|fxr|fxrev|fxb|tqr)\/.*/gi;
 
-const Components = new Map();
+const Components = new Map<string, Component>();
 Components.set('00-commentbug', {
   refRegs: [PROJECT_COMMENT_BUG_RE],
   replacer: replaceCommentBugRef,
@@ -55,18 +55,18 @@ Components.set('05-linkify-shorthand', {
 // 06-versioncontrol unused.
 
 // Replace plain text references with links functions.
-function replaceIssueRef(stringMatch, projectName, localId, commentId) {
+function replaceIssueRef(stringMatch: string, projectName: string, localId: string, commentId: string) {
   return createIssueRefRun(projectName, localId, stringMatch, commentId);
 }
 
 function replaceTrackerIssueRef(
-  match,
+  match: RegExpExecArray,
   currentProjectName = CRBUG_DEFAULT_PROJECT
 ) {
   const issueRefRE = PROJECT_LOCALID_RE;
   const commentId = '';
-  const textRuns = [];
-  let refMatch;
+  const textRuns: TextRun[] = [];
+  let refMatch: RegExpExecArray | null;
   let pos = 0;
   while ((refMatch = issueRefRE.exec(match[0])) !== null) {
     if (refMatch.index > pos) {
@@ -92,8 +92,8 @@ function replaceTrackerIssueRef(
   return textRuns;
 }
 
-function replaceCommentBugRef(match) {
-  let textRun;
+function replaceCommentBugRef(match: RegExpExecArray) {
+  let textRun: TextRun;
   const issueNum = match[7];
   const commentNum = match[18];
   if (issueNum && commentNum) {
@@ -108,8 +108,8 @@ function replaceCommentBugRef(match) {
   return [textRun];
 }
 
-function replaceLinkRef(match) {
-  const textRuns = [];
+function replaceLinkRef(match: RegExpExecArray) {
+  const textRuns: TextRun[] = [];
   let content = match[0];
   let trailing = '';
   if (match[1]) {
@@ -118,9 +118,9 @@ function replaceLinkRef(match) {
   }
   LINK_TRAILING_CHARS.forEach(([begin, end]) => {
     if (content.endsWith(end)) {
-      if (!begin || !content.slice(0, -end.length).includes(begin)) {
+      if (!begin || !content.slice(0, -end!.length).includes(begin)) {
         trailing = end + trailing;
-        content = content.slice(0, -end.length);
+        content = content.slice(0, -end!.length);
       }
     }
   });
@@ -161,7 +161,7 @@ function createIssueRefRun(projectName, localId, content, commentId) {
 export function markupAutolinks(plainString, featureLinks = []) {
   plainString = plainString || '';
   const chunks = [plainString.trim()];
-  const textRuns = [];
+  const textRuns: TextRun[] = [];
   chunks.filter(Boolean).forEach(chunk => {
     textRuns.push(...autolinkChunk(chunk));
   });
@@ -186,7 +186,7 @@ function autolinkChunk(chunk) {
 }
 
 function applyLinks(textRuns, replacer, re) {
-  const resultRuns = [];
+  const resultRuns: TextRun[] = [];
   textRuns.forEach(textRun => {
     if (textRun.tag) {
       resultRuns.push(textRun);
@@ -209,3 +209,14 @@ function applyLinks(textRuns, replacer, re) {
   });
   return resultRuns;
 }
+
+interface TextRun {
+    content: string;
+    tag?: string;
+    href?: string;
+  }
+
+  interface Component {
+    refRegs: RegExp[];
+    replacer: (match: any) => never[] | TextRun[];
+  }
