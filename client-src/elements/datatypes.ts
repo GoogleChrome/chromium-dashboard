c37b97c65a776a1d3870d117cdd3c0579b621512
Mark Xiong <markxiong0122@gmail.com>
2024-06-14 12:29:35 -0500

    Typescript migration: misc files/introduce build as an intermediate step (#3958)
    1.Introduced a new build folder for outputting compiled ts/js files
    2.migrated some misc elements to typescript:
    icons
    query field
    external reviewers
    autolink

--- client-src/elements/datatypes.ts
+++ client-src/elements/datatypes.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    google: any;
+  }
+}
+
 //chromedash-timeline
 export interface Property {
   [key: string]: any;
@@ -5,8 +11,20 @@ export interface Property {
   1: string;
 }
 
-declare global {
-  interface Window {
-    google: any;
-  }
+//autolink
+export interface TextRun {
+  content: string;
+  tag?: string;
+  href?: string;
 }
+
+export interface Component {
+  refRegs: RegExp[];
+  replacer: (match: any) => never[] | TextRun[];
+}
+
+//external-reviewers
+export type LabelInfo = {
+  description: string;
+  variant: 'primary' | 'success' | 'neutral' | 'warning' | 'danger';
+};
