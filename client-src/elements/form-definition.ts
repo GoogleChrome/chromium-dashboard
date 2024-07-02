7847bb775fec8ad7dc82d04e5a675068e7ad61c5
Mark Xiong <markxiong0122@gmail.com>
2024-07-02 14:27:44 -0500

    Moved type Feature into cs-client  (#4048)
    moved type Feature into cs-client

--- client-src/elements/form-definition.ts
+++ client-src/elements/form-definition.ts
@@ -1,68 +1,6 @@
 import {html, TemplateResult} from 'lit';
+import {Feature} from '../js-src/cs-client';
 import * as enums from './form-field-enums';
-
-interface BaseBrowser {
-  view: {
-    val: string;
-    text: string;
-    url: string;
-    notes: string;
-  };
-}
-
-interface ChromeBrowser extends BaseBrowser {
-  bug: string;
-  blink_components: string[];
-  devrel: string[];
-  owners: string[];
-  prefixed: boolean;
-  status: {val: string};
-  desktop: string;
-  android: string;
-  webview: string;
-  ios: string;
-}
-
-interface FirefoxBrowser extends BaseBrowser {}
-interface SafariBrowser extends BaseBrowser {}
-interface WebDevBrowser extends BaseBrowser {}
-interface OtherBrowser {
-  view: {notes: string};
-}
-
-//resources and standards used in Feature object
-interface Resources {
-  docs: string[];
-  samples: string[];
-}
-
-interface Standards {
-  spec: string;
-  maturity: {val: number; text: string};
-}
-
-type FeatureName = string;
-
-interface Feature {
-  name?: FeatureName;
-  category_int: number;
-  enterprise_feature_categories: string[];
-  feature_type_int: number;
-  intent_stage_int: number;
-  standards: Standards;
-  tag_review_status_int: number;
-  security_review_status_int: number;
-  privacy_review_status_int: number;
-  resources: Resources;
-  tags: string[];
-  browsers: {
-    chrome: ChromeBrowser;
-    ff: FirefoxBrowser;
-    safari: SafariBrowser;
-    webdev: WebDevBrowser;
-    other: OtherBrowser;
-  };
-}
 
 interface FormattedFeature {
   category: number;
@@ -70,34 +8,34 @@ interface FormattedFeature {
   feature_type: number;
   intent_stage: number;
   accurate_as_of: boolean;
-  spec_link: string;
+  spec_link?: string;
   standard_maturity: number;
-  tag_review_status: number;
-  security_review_status: number;
-  privacy_review_status: number;
+  tag_review_status?: number;
+  security_review_status?: number;
+  privacy_review_status?: number;
   sample_links: string[];
   doc_links: string[];
   search_tags: string[];
-  blink_components: string;
-  bug_url: string;
-  devrel: string[];
-  owner: string[];
-  prefixed: boolean;
-  impl_status_chrome: string;
-  shipped_milestone: string;
-  shipped_android_milestone: string;
-  shipped_webview_milestone: string;
-  shipped_ios_milestone: string;
-  ff_views: string;
-  ff_views_link: string;
-  ff_views_notes: string;
-  safari_views: string;
-  safari_views_link: string;
-  safari_views_notes: string;
-  web_dev_views: string;
-  web_dev_views_link: string;
-  web_dev_views_notes: string;
-  other_views_notes: string;
+  blink_components?: string;
+  bug_url?: string;
+  devrel?: string[];
+  owner?: string[];
+  prefixed?: boolean;
+  impl_status_chrome?: string;
+  shipped_milestone?: number;
+  shipped_android_milestone?: number;
+  shipped_webview_milestone?: number;
+  shipped_ios_milestone?: number;
+  ff_views?: number;
+  ff_views_link?: string;
+  ff_views_notes?: string;
+  safari_views?: number;
+  safari_views_link?: string;
+  safari_views_notes?: string;
+  web_dev_views?: number;
+  web_dev_views_link?: string;
+  web_dev_views_notes?: string;
+  other_views_notes?: string;
   [key: string]: any; // Allow additional properties
 }
 
@@ -165,7 +103,8 @@ export function formatFeatureForEdit(feature: Feature): FormattedFeature {
     search_tags: feature.tags,
 
     // from feature.browsers.chrome
-    blink_components: feature.browsers.chrome.blink_components[0],
+    blink_components: feature.browsers.chrome.blink_components?.[0],
+
     bug_url: feature.browsers.chrome.bug,
     devrel: feature.browsers.chrome.devrel,
     owner: feature.browsers.chrome.owners,
@@ -177,22 +116,22 @@ export function formatFeatureForEdit(feature: Feature): FormattedFeature {
     shipped_ios_milestone: feature.browsers.chrome.ios,
 
     // from feature.browsers.ff
-    ff_views: feature.browsers.ff.view.val,
-    ff_views_link: feature.browsers.ff.view.url,
-    ff_views_notes: feature.browsers.ff.view.notes,
+    ff_views: feature.browsers.ff.view?.val,
+    ff_views_link: feature.browsers.ff.view?.url,
+    ff_views_notes: feature.browsers.ff.view?.notes,
 
     // from feature.browsers.safari
-    safari_views: feature.browsers.safari.view.val,
-    safari_views_link: feature.browsers.safari.view.url,
-    safari_views_notes: feature.browsers.safari.view.notes,
+    safari_views: feature.browsers.safari.view?.val,
+    safari_views_link: feature.browsers.safari.view?.url,
+    safari_views_notes: feature.browsers.safari.view?.notes,
 
     // from feature.browsers.webdev
-    web_dev_views: feature.browsers.webdev.view.val,
-    web_dev_views_link: feature.browsers.webdev.view.url,
-    web_dev_views_notes: feature.browsers.webdev.view.notes,
+    web_dev_views: feature.browsers.webdev.view?.val,
+    web_dev_views_link: feature.browsers.webdev.view?.url,
+    web_dev_views_notes: feature.browsers.webdev.view?.notes,
 
     // from feature.browsers.other
-    other_views_notes: feature.browsers.other.view.notes,
+    other_views_notes: feature.browsers.other.view?.notes,
   };
 
   COMMA_SEPARATED_FIELDS.map(field => {
