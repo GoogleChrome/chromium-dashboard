69d2037ef9149c8001693d0e303851bd60c16816
markxiong0122 <markxiong0122@gmail.com>
2024-07-01 22:08:50 -0500

    feature data type

--- client-src/js-src/cs-client.js
+++ client-src/js-src/cs-client.js
@@ -42,6 +42,236 @@
  */
 
 /**
+ * @typedef {Object} StageDict
+ * @property {number} id
+ * @property {string} created
+ * @property {number} feature_id
+ * @property {number} stage_type
+ * @property {string} display_name
+ * @property {number} intent_stage
+ * @property {string|null} intent_thread_url
+ * @property {string|null} announcement_url
+ * @property {string|null} origin_trial_id
+ * @property {string|null} experiment_goals
+ * @property {string|null} experiment_risks
+ * @property {StageDict[]} extensions
+ * @property {string|null} origin_trial_feedback_url
+ * @property {boolean} ot_action_requested
+ * @property {string|null|undefined} ot_activation_date
+ * @property {number|null} ot_approval_buganizer_component
+ * @property {string|null} ot_approval_criteria_url
+ * @property {string|null} ot_approval_group_email
+ * @property {string|null} ot_chromium_trial_name
+ * @property {string|null} ot_description
+ * @property {string|null} ot_display_name
+ * @property {string|null} ot_documentation_url
+ * @property {string[]} ot_emails
+ * @property {string|null} ot_feedback_submission_url
+ * @property {boolean} ot_has_third_party_support
+ * @property {boolean} ot_is_critical_trial
+ * @property {boolean} ot_is_deprecation_trial
+ * @property {string|null} ot_owner_email
+ * @property {boolean} ot_require_approvals
+ * @property {number|undefined} ot_setup_status
+ * @property {string|null|undefined} ot_request_note
+ * @property {number|null} ot_stage_id
+ * @property {string|null} experiment_extension_reason
+ * @property {string|null} finch_url
+ * @property {string|null} rollout_details
+ * @property {number|null} rollout_impact
+ * @property {number|null} rollout_milestone
+ * @property {string[]} rollout_platforms
+ * @property {string[]} enterprise_policies
+ * @property {string[]} pm_emails
+ * @property {string[]} tl_emails
+ * @property {string[]} ux_emails
+ * @property {string[]} te_emails
+ * @property {number|null} desktop_first
+ * @property {number|null} android_first
+ * @property {number|null} ios_first
+ * @property {number|null} webview_first
+ * @property {number|null} desktop_last
+ * @property {number|null} android_last
+ * @property {number|null} ios_last
+ * @property {number|null} webview_last
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerResourceInfo
+ * @property {string[]} samples
+ * @property {string[]} docs
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerMaturityInfo
+ * @property {string|null} text
+ * @property {string|null} short_text
+ * @property {number} val
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerStandardsInfo
+ * @property {string|null} spec
+ * @property {FeatureDictInnerMaturityInfo} maturity
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerBrowserStatus
+ * @property {string|null} text
+ * @property {string|null} val
+ * @property {string|null} milestone_str
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerViewInfo
+ * @property {string|null} text
+ * @property {number|null} val
+ * @property {string|null} url
+ * @property {string|null} notes
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerChromeBrowserInfo
+ * @property {string|null} bug
+ * @property {string[]|null} blink_components
+ * @property {string[]|null} devrel
+ * @property {string[]|null} owners
+ * @property {boolean|null} origintrial
+ * @property {boolean|null} intervention
+ * @property {boolean|null} prefixed
+ * @property {boolean|null} flag
+ * @property {FeatureDictInnerBrowserStatus} status
+ * @property {number|null} desktop
+ * @property {number|null} android
+ * @property {number|null} webview
+ * @property {number|null} ios
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerSingleBrowserInfo
+ * @property {FeatureDictInnerViewInfo|null} view
+ */
+
+/**
+ * @typedef {Object} FeatureBrowsersInfo
+ * @property {FeatureDictInnerChromeBrowserInfo} chrome
+ * @property {FeatureDictInnerSingleBrowserInfo} ff
+ * @property {FeatureDictInnerSingleBrowserInfo} safari
+ * @property {FeatureDictInnerSingleBrowserInfo} webdev
+ * @property {FeatureDictInnerSingleBrowserInfo} other
+ */
+
+/**
+ * @typedef {Object} FeatureDictInnerUserEditInfo
+ * @property {string|null} by
+ * @property {string|null} when
+ */
+
+/**
+ * @typedef {Object} Feature
+ * Metadata: Creation and updates
+ * @property {number} id
+ * @property {FeatureDictInnerUserEditInfo} created
+ * @property {FeatureDictInnerUserEditInfo} updated
+ * @property {string|null} accurate_as_of
+ * @property {string|null} creator_email
+ * @property {string|null} updater_email
+ * Metadata: Access controls
+ * @property {string[]} owner_emails
+ * @property {string[]} editor_emails
+ * @property {string[]} cc_emails
+ * @property {string[]} spec_mentor_emails
+ * @property {boolean} unlisted
+ * @property {boolean} deleted
+ * Renamed metadata fields
+ * @property {string[]} editors
+ * @property {string[]} cc_recipients
+ * @property {string[]} spec_mentors
+ * @property {string|null} creator
+ * Descriptive info.
+ * @property {string} name
+ * @property {string} summary
+ * @property {string} category
+ * @property {number} category_int
+ * @property {string[]} blink_components
+ * @property {number} star_count
+ * @property {string[]} search_tags
+ * @property {string|null} feature_notes
+ * @property {string[]} enterprise_feature_categories
+ * Metadata: Process information
+ * @property {string} feature_type
+ * @property {number} feature_type_int
+ * @property {string} intent_stage
+ * @property {number} intent_stage_int
+ * @property {number|null} active_stage_id
+ * @property {string|null} bug_url
+ * @property {string|null} launch_bug_url
+ * @property {string[]} screenshot_links
+ * @property {number|null} first_enterprise_notification_milestone
+ * @property {boolean} breaking_change
+ * @property {number} enterprise_impact
+ * Implementation in Chrome
+ * @property {string|null} flag_name
+ * @property {string|null} finch_name
+ * @property {string|null} non_finch_justification
+ * @property {string|null} ongoing_constraints
+ * Topic: Adoption
+ * @property {string|null} motivation
+ * @property {string|null} devtrial_instructions
+ * @property {string|null} activation_risks
+ * @property {string|null} measurement
+ * @property {string|null} availability_expectation
+ * @property {string|null} adoption_expectation
+ * @property {string|null} adoption_plan
+ * Gate: Standardization and Interop
+ * @property {string|null} initial_public_proposal_url
+ * @property {string[]} explainer_links
+ * @property {boolean} requires_embedder_support
+ * @property {string|null} spec_link
+ * @property {string|null} api_spec
+ * @property {boolean|null} prefixed
+ * @property {string|null} interop_compat_risks
+ * @property {boolean|null} all_platforms
+ * @property {boolean|null} all_platforms_descr
+ * @property {string|null} tag_review
+ * @property {string|null} non_oss_deps
+ * @property {string|null} anticipated_spec_changes
+ * Gate: Security & Privacy
+ * @property {string|null} security_risks
+ * @property {string[]} tags
+ * @property {string} tag_review_status
+ * @property {number|null} tag_review_status_int
+ * @property {string} security_review_status
+ * @property {number|null} security_review_status_int
+ * @property {string} privacy_review_status
+ * @property {number|null} privacy_review_status_int
+ * Gate: Testing / Regressions
+ * @property {string|null} ergonomics_risks
+ * @property {boolean|null} wpt
+ * @property {string|null} wpt_descr
+ * @property {string|null} webview_risks
+ * Gate: Devrel & Docs
+ * @property {string[]} devrel_emails
+ * @property {string|null} debuggability
+ * @property {string[]} doc_links
+ * @property {string[]} sample_links
+ * @property {StageDict[]} stages
+ * @property {string|null} experiment_timeline
+ * @property {FeatureDictInnerResourceInfo} resources
+ * @property {string|null} comments
+ * Repeated in 'browsers' section. TODO(danielrsmith): delete these?
+ * @property {number} ff_views
+ * @property {number} safari_views
+ * @property {number} web_dev_views
+ * @property {FeatureBrowsersInfo} browsers
+ * @property {FeatureDictInnerStandardsInfo} standards
+ * @property {boolean} is_released
+ * @property {boolean} is_enterprise_feature
+ * @property {string|null} updated_display
+ * @property {string|null} new_crbug_url
+ */
+
+/**
  * Generic Chrome Status Http Error.
  */
 class ChromeStatusHttpError extends Error {
