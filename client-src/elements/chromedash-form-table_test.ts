59de904673a0f6dfdd7b2a4adf65f825bf0e149e
markxiong0122 <markxiong0122@gmail.com>
2024-07-02 22:12:12 -0500

    form table

--- client-src/elements/chromedash-form-table_test.ts
+++ client-src/elements/chromedash-form-table_test.ts
@@ -1,5 +1,5 @@
-import {html} from 'lit';
 import {assert, fixture} from '@open-wc/testing';
+import {html} from 'lit';
 import {ChromedashFormTable} from './chromedash-form-table';
 import {slotAssignedElements} from './utils';
 
@@ -10,12 +10,12 @@ describe('chromedash-form-table', () => {
     );
     assert.exists(component);
     assert.instanceOf(component, ChromedashFormTable);
-    const formContents = component.shadowRoot.querySelector('*');
+    const formContents = component.renderRoot.querySelector('*');
     assert.exists(formContents);
   });
 
   it('renders with array of slots for rows', async () => {
-    const component = await fixture(
+    const component: ChromedashFormTable = await fixture(
       html` <chromedash-form-table>
         <span>Row 1</span>
         <span>Row 2</span>
