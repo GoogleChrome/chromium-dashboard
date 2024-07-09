07120c9204f6a410cd8ec7f8ec32d9eb6ae7bdc2
Jeffrey Yasskin <jyasskin@google.com>
2024-07-09 15:39:33 -0700

    Explain where to host specifications.

--- client-src/elements/form-field-specs.ts
+++ client-src/elements/form-field-specs.ts
@@ -748,6 +748,16 @@ export const ALL_FIELDS: Record<string, Field> = {
     help_text: html` Link to the spec, if and when available. When implementing
     a spec update, please link to a heading in a published spec rather than a
     pull request when possible.`,
+    extra_help: html`<p>
+      Specifications should be written in the format and hosted in the URL space
+      expected by your target standards body. For example, the W3C expects
+      <a href="https://respec.org/" target="_blank">Respec</a> or
+      <a href="https://speced.github.io/bikeshed/" target="_blank">Bikeshed</a>
+      hosted on w3.org or in Github Pages. The IETF expects an
+      <a href="https://authors.ietf.org/" target="_blank">Internet-Draft</a>
+      hosted in the
+      <a href="https://datatracker.ietf.org/" target="_blank">Datatracker</a>.
+    </p>`,
     check: value =>
       checkNotGoogleDocs(
         value,
