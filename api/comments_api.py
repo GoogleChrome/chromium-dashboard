4fc003205fa63581fbc19eef534c63ab8e915b5b
markxiong0122 <markxiong0122@gmail.com>
2024-07-30 23:08:00 -0500

    add models

--- api/comments_api.py
+++ api/comments_api.py
@@ -13,6 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from chromestatus_openapi.models import (
+    Activity as ActivityModel,
+    Amendment as AmendmentModel,
+    CommentRequest,
+    PatchCommentRequest,
+    GetCommentsResponse,
+    SuccessMessage,
+    ErrorMessage,
+)
 from typing import Any
 
 from framework import basehandlers
@@ -24,28 +33,28 @@ from internals import notifier_helpers
 from internals import slo
 
 
-def amendment_to_json_dict(amendment: Amendment) -> dict[str, Any]:
-  return {
-      'field_name': amendment.field_name,
-      'old_value': amendment.old_value.strip('[]'),
-      'new_value': amendment.new_value.strip('[]'),
-      }
+def amendment_to_json_dict(amendment: Amendment) -> AmendmentModel:
+  return AmendmentModel(
+      field_name = amendment.field_name,
+      old_value = amendment.old_value.strip('[]'),
+      new_value = amendment.new_value.strip('[]'),
+  ).to_dict()
 
 
 def activity_to_json_dict(comment: Activity) -> dict[str, Any]:
   amendments_json = [
       amendment_to_json_dict(amnd) for amnd in comment.amendments
       if amnd.old_value != 'None' or amnd.new_value != '[]']
-  return {
-      'comment_id': comment.key.id(),
-      'feature_id': comment.feature_id,
-      'gate_id': comment.gate_id,
-      'created': str(comment.created),  # YYYY-MM-DD HH:MM:SS.SSS
-      'author': comment.author,
-      'content': comment.content,
-      'deleted_by': comment.deleted_by,
-      'amendments': amendments_json,
-      }
+  return ActivityModel(
+      comment_id = comment.key.id(),
+      feature_id = comment.feature_id,
+      gate_id = comment.gate_id,
+      created = str(comment.created),  # YYYY-MM-DD HH:MM:SS.SSS
+      author = comment.author,
+      content = comment.content,
+      deleted_by = comment.deleted_by,
+      amendments = amendments_json,
+  ).to_dict()
 
 
 class CommentsAPI(basehandlers.APIHandler):
@@ -72,7 +81,7 @@ class CommentsAPI(basehandlers.APIHandler):
       lambda c: self._should_show_comment(c, user_email, is_admin), comments))
 
     dicts = [activity_to_json_dict(c) for c in comments]
-    return {'comments': dicts}
+    return GetCommentsResponse(comments=dicts).to_dict()
 
   def do_post(self, **kwargs) -> dict[str, str]:
     """Add a review comment and possibly set a approval value."""
@@ -114,22 +123,21 @@ class CommentsAPI(basehandlers.APIHandler):
           feature, gate_id, post_to_thread_type, user.email(), comment_content)
 
     # Callers don't use the JSON response for this API call.
-    return {'message': 'Done'}
+    return SuccessMessage(message='Done').to_dict()
 
   def do_patch(self, **kwargs) -> dict[str, str]:
-    comment_id = self.get_param('commentId', required=True)
-    comment: Activity = Activity.get_by_id(comment_id)
+    patch_request = PatchCommentRequest(**self.request.json)
+    comment: Activity = Activity.get_by_id(patch_request.commentId)
 
     user = self.get_current_user(required=True)
     if not permissions.can_admin_site(user) and (
         comment and user.email() != comment.author):
       self.abort(403, msg='User does not have comment edit permissions')
 
-    is_undelete = self.get_param('isUndelete', required=True)
-    if is_undelete:
+    if patch_request.isUndelete:
       comment.deleted_by = None
     else:
       comment.deleted_by = user.email()
     comment.put()
 
-    return {'message': 'Done'}
+    return SuccessMessage(message='Done').to_dict()
