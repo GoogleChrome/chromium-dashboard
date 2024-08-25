6eb86e1b4e08c8794111863b47dff027f37e1e8b
markxiong0122 <markxiong0122@gmail.com>
2024-08-25 16:33:43 +0800

    crossing todos

--- client-src/elements/chromedash-roadmap.ts
+++ client-src/elements/chromedash-roadmap.ts
@@ -2,11 +2,10 @@ import '@polymer/iron-icon';
 import {LitElement, css, html} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 import {SHARED_STYLES} from '../css/shared-css.js';
-import {Feature} from '../js-src/cs-client.js';
+import {Channels, Feature} from '../js-src/cs-client.js';
 import {TemplateContent} from './chromedash-roadmap-milestone-card.js';
 import {showToastMessage} from './utils.js';
 
-//TODO(markxiong0122): move this to cs-client.js after converting it to TypeScript
 interface MilestoneDetails {
   branch_point: string;
   earliest_beta: string;
@@ -105,7 +104,7 @@ export class ChromedashRoadmap extends LitElement {
   @property({type: Number, attribute: false})
   cardWidth = 0;
   @state()
-  channels; //TODO(markxiong0122): Type this as Channel when PR#4085 is merged
+  channels: Channels = {};
   @state()
   starredFeatures = new Set<number>();
   /**
@@ -314,7 +313,7 @@ export class ChromedashRoadmap extends LitElement {
         milestone => html`
           <chromedash-roadmap-milestone-card
             style="width:${this.cardWidth}px;"
-            .channel=${this.milestoneInfo?.[milestone]}
+            .channel=${this.milestoneInfo[milestone]}
             .templateContent=${TEMPLATE_CONTENT['stable_minus_one']}
             ?showdates=${SHOW_DATES}
             .starredFeatures=${this.starredFeatures}
@@ -330,7 +329,7 @@ export class ChromedashRoadmap extends LitElement {
         type => html`
           <chromedash-roadmap-milestone-card
             style="width:${this.cardWidth}px;"
-            .channel=${this.channels?.[type]}
+            .channel=${this.channels[type]}
             .templateContent=${TEMPLATE_CONTENT[type]}
             ?showdates=${SHOW_DATES}
             .starredFeatures=${this.starredFeatures}
@@ -346,7 +345,7 @@ export class ChromedashRoadmap extends LitElement {
         milestone => html`
           <chromedash-roadmap-milestone-card
             style="width:${this.cardWidth}px;"
-            .channel=${this.milestoneInfo?.[milestone]}
+            .channel=${this.milestoneInfo[milestone]}
             .templateContent=${TEMPLATE_CONTENT['dev_plus_one']}
             ?showdates=${SHOW_DATES}
             .starredFeatures=${this.starredFeatures}
