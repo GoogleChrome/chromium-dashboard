7ad040bdbeb3a9cab38bfc48c941db5ba1e505fa
Jason Robbins <jrobbins@gmail.com>
2024-10-23 20:23:09 +0000

    Switch localhost development to port 8888.

--- gen/py/chromestatus_openapi/Dockerfile
+++ gen/py/chromestatus_openapi/Dockerfile
@@ -9,7 +9,7 @@ RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . /usr/src/app
 
-EXPOSE 8080
+EXPOSE 8888
 
 ENTRYPOINT ["python3"]
 
--- packages/playwright/playwright.config.cjs
+++ packages/playwright/playwright.config.cjs
@@ -30,7 +30,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:8888',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -98,7 +98,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080',
+    url: 'http://localhost:8888',
     reuseExistingServer: true,
   },
 });
