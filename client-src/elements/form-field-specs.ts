600737382e79f31bb5ec7e32c9474fc0eedfce01
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2025-01-13 18:09:19 +0000

    Add clarification about mailing lists as contacts for OT creation (#4680)
    * add clarification about mailing lists
    
    * lint
    
    ---------
    
    Co-authored-by: DanielRyanSmith <danielrsmith@google.com>

--- client-src/elements/form-field-specs.ts
+++ client-src/elements/form-field-specs.ts
@@ -1461,7 +1461,12 @@ export const ALL_FIELDS: Record<string, Field> = {
     required: false,
     label: 'Origin trial contacts',
     help_text: html` List any other individuals or groups to include on the
-      contact list (e.g. for reminders on trial milestones).
+      contact list (e.g. for reminders on trial milestones). Mailing list emails
+      can be used here, but only email addresses of individuals will receive
+      access to view registrant data on
+      <a target="_blank" href="http://go/ot-registrants-dashboard"
+        >go/ot-registrants-dashboard</a
+      >.
       <p>
         <strong>
           Please prefer using "@google.com" domain email addresses for any
