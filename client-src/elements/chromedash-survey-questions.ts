b5c00191a71b60ef12dd8081b9cacb45a89916f0
Jason Robbins <jrobbins@gmail.com>
2025-01-20 22:22:04 +0000

    progress

--- client-src/elements/chromedash-survey-questions.ts
+++ client-src/elements/chromedash-survey-questions.ts
@@ -3,6 +3,7 @@ import {customElement, property, state} from 'lit/decorators.js';
 import {SHARED_STYLES} from '../css/shared-css.js';
 import {Feature, StageDict, User} from '../js-src/cs-client';
 import {GateDict} from './chromedash-gate-chip';
+import {GATE_TYPES} from './form-field-enums.js';
 import {GATE_QUESTIONNAIRES} from './gate-details.js';
 import {autolink} from './utils.js';
 
@@ -24,9 +25,16 @@ export class ChromedashGateColumn extends LitElement {
         #questionnaire ul {
           padding-left: 1em;
         }
-        #questionnaire li {
+        #questionnaire ul li {
           list-style: disc;
-        }
+      }
+      #questionnaire ol li {
+      list-style: auto;
+      margin-left: var(--content-padding);
+      }
+      .question {
+          padding: var(--content-padding-half);
+      }
         .instructions {
           padding: var(--content-padding-half);
           margin-bottom: var(--content-padding-large);
@@ -49,14 +57,56 @@ export class ChromedashGateColumn extends LitElement {
 
   renderQuestionnaireSkeleton(): TemplateResult {
     return html`
-      <h2>Survey questions</h2>
       <!-- prettier-ignore -->
       <div id="questionnaire">Loading...</div>
       <p class="instructions">&nbsp;</p>
     `;
   }
 
+  renderBooleanField(name: string, desc: string): TemplateResult {
+    return html`
+    <li class="question">
+    <sl-checkbox name="${name}"></sl-checkbox>
+    ${desc}
+    </li>
+    `;
+  }
+
+  renderStringField(name: string, desc: string): TemplateResult {
+    return html`
+    <li class="question">
+    ${desc}
+    <sl-input name="${name}" size="small"></sl-input>
+    </li>
+    `;
+  }
+
+  renderPrivacyForm(): TemplateResult {
+    return html`
+    <div id="questionnaire">
+    <ol>
+      ${this.renderBooleanField(
+        "is_language_polyfill",
+        "a long description that takes up more than one full line.")}
+      ${this.renderBooleanField(
+        "field",
+        "desc")}
+      ${this.renderBooleanField(
+        "field",
+        "desc")}
+      ${this.renderStringField(
+        "field",
+        "desc that kind of goes a long way")}
+        </ol>
+    </div>
+    `;
+  }
+
   renderQuestionnaire(): TemplateResult {
+    if (this.gate.gate_type === GATE_TYPES.PRIVACY_ORIGIN_TRIAL ||
+      this.gate.gate_type === GATE_TYPES.PRIVACY_SHIP) {
+        return this.renderPrivacyForm();
+    }
     const questionnaireText = GATE_QUESTIONNAIRES[this.gate.gate_type];
     if (!questionnaireText) return html``;
     const markup =
@@ -64,7 +114,6 @@ export class ChromedashGateColumn extends LitElement {
         ? autolink(questionnaireText)
         : questionnaireText;
     return html`
-      <h2>Survey questions</h2>
       <!-- prettier-ignore -->
       <div id="questionnaire">${markup}</div>
       <p class="instructions">Please post responses in the comments below.</p>
@@ -72,11 +121,12 @@ export class ChromedashGateColumn extends LitElement {
   }
 
   render(): TemplateResult {
-    if (this.loading) {
-      return this.renderQuestionnaireSkeleton();
-    } else {
-      return this.renderQuestionnaire();
-    }
+    return html`
+    <h2>Survey questions</h2>
+    ${this.loading ?
+      this.renderQuestionnaireSkeleton() :
+      this.renderQuestionnaire()}
+    `;
   }
 
 }
