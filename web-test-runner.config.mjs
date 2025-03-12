0cf2e0f619d54c0449f3a7e7143b8bbce427ae9d
Jason Robbins <jrobbins@gmail.com>
2025-03-12 21:34:58 +0000

    try concurrency: 4

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,7 @@
 export default {
   files: 'build/**/*_test.{js,ts}',
+  concurrentBrowsers: 4,
+  concurrency: 4,
   nodeResolve: true,
-  browserStartTimeout: 120000,
+  browserStartTimeout: 60000,
 };
