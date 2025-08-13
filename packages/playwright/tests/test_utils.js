8283b684ea731f66d64c64204676d65532a0bee5
Jason Robbins <jrobbins@gmail.com>
2025-08-13 17:30:39 +0000

    Wait for page content to appear

--- packages/playwright/tests/test_utils.js
+++ packages/playwright/tests/test_utils.js
@@ -364,7 +364,8 @@ export async function createNewFeature(page) {
 
   // Wait until we are on the Feature page.
   await page.waitForURL('**/feature/*');
-  await delay(2500);
+  await delay(1500);
+  await page.locator('chromedash-feature-detail');
 }
 
 export async function gotoNewFeatureList(page) {
