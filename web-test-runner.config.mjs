c0c8d75ba346f702f96d924944b5c29517d13c3e
DanielRyanSmith <56164590+DanielRyanSmith@users.noreply.github.com>
2025-10-16 00:12:42 +0000

    supress dev mode warnings while testing

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -4,4 +4,13 @@ export default {
   concurrency: 1,
   nodeResolve: true,
   browserStartTimeout: 60000,
+  filterBrowserLogs(log) {
+      // Suppress the "Lit is in dev mode" warning
+      if (log.args.some((arg) => typeof arg === 'string' && arg.includes('Lit is in dev mode'))) {
+        return false;
+      }
+
+      // Allow all other logs to pass through
+      return true;
+    },
 };
