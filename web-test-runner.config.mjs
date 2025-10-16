2bed3c107407229c12003a67c33284e437d62134
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2025-10-16 19:32:11 +0000

    Supress dev mode warnings while testing (#5566)
    * supress dev mode warnings while testing
    
    * Keep a list of filtered logs

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,7 +1,20 @@
+const filteredLogs = [
+  'Lit is in dev mode',
+];
+
 export default {
   files: 'build/**/*_test.{js,ts}',
   concurrentBrowsers: 1,
   concurrency: 1,
   nodeResolve: true,
   browserStartTimeout: 60000,
+  filterBrowserLogs(log) {
+    for (const arg of log.args) {
+      if (typeof arg === 'string' && filteredLogs.some(l => arg.includes(l))) {
+        return false;
+      }
+    }
+
+    return true;
+  },
 };
