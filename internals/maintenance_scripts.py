9d1370fb6de3c5fd523f8875597f98643c112d64
DanielRyanSmith <56164590+DanielRyanSmith@users.noreply.github.com>
2025-10-17 18:20:39 +0000

    small change to query

--- internals/maintenance_scripts.py
+++ internals/maintenance_scripts.py
@@ -1018,15 +1018,16 @@ class GenerateStaleFeaturesFile(FlaskHandler):
     # Get all features that have not been verified for accuracy in over a month.
     now = datetime.now()
     one_month_ago = now - timedelta(weeks=4)
-    stale_features = FeatureEntry.query(
+    stale_features: list[FeatureEntry] = FeatureEntry.query(
       ndb.OR(
         FeatureEntry.accurate_as_of < one_month_ago,
         FeatureEntry.accurate_as_of == None,
       ),
-    )
+    ).fetch()
+
     # We should only surface features we have sent notifications about.
     # (This cannot be added to the query, since only 1 inequality is allowed per query.)
-    stale_features = [f for f in stale_features if f.outstanding_notifications >= 1]
+    stale_features = [f for f in stale_features if f.outstanding_notifications > 0]
     stale_features_with_upcoming_ship_stages: list[FeatureEntry] = []
     for f in stale_features:
       shipping_stage_type = STAGE_TYPES_SHIPPING[f.feature_type]
