65f12a092f1f43d1d0a496187b804dfef9663410
DanielRyanSmith <56164590+DanielRyanSmith@users.noreply.github.com>
2025-11-10 03:33:32 +0000

    New endpoint and cloud task to run WPT coverage eval pipeline

--- framework/gemini_helpers_test.py
+++ framework/gemini_helpers_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from internals import core_enums
 import testing_config  # Must be imported before the module under test.
 
 import asyncio
