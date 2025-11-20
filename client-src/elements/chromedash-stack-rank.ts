a4d0a4e25cfd3685b5def422a223beb4c363bf84
Jason Robbins <jrobbins@gmail.com>
2025-11-20 00:07:05 +0000

    Fix icon library name.

--- client-src/elements/chromedash-stack-rank.ts
+++ client-src/elements/chromedash-stack-rank.ts
@@ -275,7 +275,7 @@ class ChromedashStackRank extends LitElement {
                 href="#${item.property_name}"
                 @click=${this.scrollToPosition}
               >
-                <sl-icon class="hash-link" name="link"></sl-icon>
+                <sl-icon class="hash-link" library="material" name="link"></sl-icon>
                 <p>${item.property_name}</p>
               </a>
             </div>
